5c6f1705c81ea3d5ece21c81af922a26caf2cf5c
Wing Ming Chan <chanw@upstate.edu>
2016-09-01 13:19:12 -0400

    Added XML documentation

--- asset_classes/Container.class.php
+++ asset_classes/Container.class.php
@@ -12,16 +12,35 @@
 namespace cascade_ws_asset;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
-use cascade_ws_property as p;
+use cascade_ws_property  as p;
 
+/**
+<documentation>
+<description><h2>Introduction</h2>
+<p>The <code>Container</code> class is the superclass of <code>AssetFactoryContainer</code>,
+<code>ConnectorContainer</code>, <code>ContentTypeContainer</code>, <code>DataDefinitionContainer</code>, <code>Folder</code>,
+<code>MetadataSetContainer</code>, <code>PageConfigurationSetContainer</code>, <code>PublishSetContainer</code>,
+<code>SiteDestinationContainer</code>, <code>TransportContainer</code>, and <code>WorkflowDefinitionContainer</code>.
+It is an abstract class and defines all methods commonly shared by its sub-classes.</p>
+</description>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/container.php">container.php</a></li></ul></postscript>
+</documentation>
+*/
 abstract class Container extends ContainedAsset
 {
     const DEBUG = false;
 
-    public function __construct( aohs\AssetOperationHandlerService $service, \stdClass $identifier )
+/**
+<documentation><description><p>The constructor, overriding the parent method to process child assets.</p></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
+    protected function __construct( aohs\AssetOperationHandlerService $service, \stdClass $identifier )
     {
         parent::__construct( $service, $identifier );
         
@@ -31,12 +50,28 @@ abstract class Container extends ContainedAsset
         }
     }
     
-    public function contains( Asset $asset )
+/**
+<documentation><description><p>An alias of <code>isAncestorOf( Asset $asset )</code>.</p></description>
+<example></example>
+<return-type>bool</return-type>
+<exception></exception>
+</documentation>
+*/
+    public function contains( Asset $asset ) : bool
     {
         return $this->isAncestorOf( $asset );
     }
 
-    public function deleteAllChildren()
+/**
+<documentation><description><p>Deletes all children in the container and returns the object.
+This method uses the <code>aohs\AssetOperationHandlerService::batch</code> operation.
+There is no need to call <code>Container::edit</code> because the container object itself is not modified.</p></description>
+<example>$folder->deleteAllChildren();</example>
+<return-type>Asset</return-type>
+<exception></exception>
+</documentation>
+*/
+    public function deleteAllChildren() : Asset
     {
         if( count( $this->children ) == 0 )
         {
@@ -64,6 +99,13 @@ abstract class Container extends ContainedAsset
         return $this;
     }
 
+/**
+<documentation><description><p>Edits and returns the calling object.</p></description>
+<example>$folder->getMetadata()->setDisplayName( "Test" )->getHostAsset()->edit();</example>
+<return-type>Asset</return-type>
+<exception>EditingFailureException</exception>
+</documentation>
+*/
     public function edit(
         p\Workflow $wf=NULL, 
         WorkflowDefinition $wd=NULL, 
@@ -97,22 +139,52 @@ abstract class Container extends ContainedAsset
         return $this->reloadProperty();
     }
     
-    public function getAssetTree()
+/**
+<documentation><description><p>Returns an <a href="http://www.upstate.edu/cascade-admin/web-services/api/asset-tree/index.php"><code>AssetTree</code></a> object rooted at this container.</p></description>
+<example>u\DebugUtility::dump( $folder->getAssetTree() );</example>
+<return-type>AssetTree</return-type>
+<exception></exception>
+</documentation>
+*/
+    public function getAssetTree() : AssetTree
     {
         return new AssetTree( $this );
     }
     
+/**
+<documentation><description><p>Returns an array of <a href="http://www.upstate.edu/cascade-admin/web-services/api/property-classes/child.php"><code>p\Child</code></a> objects.</p></description>
+<example>u\DebugUtility::dump( $folder->getChildren() );</example>
+<return-type>mixed</return-type>
+<exception></exception>
+</documentation>
+*/
     public function getChildren()
     {
         return $this->children;
     }
 
-    public function getContainerChildrenIds()
+/**
+<documentation><description><p>Returns an array of id strings of child containers of the same type.
+For example, when this method is called through an <a href="http://www.upstate.edu/cascade-admin/web-services/api/asset-classes/asset-factory-container.php"><code>AssetFactoryContainer</code></a> object,
+then the id's returned are id's of all children of the type asset factory containers of the current container.</p></description>
+<example>u\DebugUtility::dump( $folder->getContainerChildrenIds() );</example>
+<return-type>array</return-type>
+<exception></exception>
+</documentation>
+*/
+    public function getContainerChildrenIds() : array
     {
         return $this->container_children_ids;
     }
     
-    public function isAncestorOf( Asset $asset )
+/**
+<documentation><description><p>Returns a bool, indicating whether this container is an ancestor of the named asset.</p></description>
+<example>echo u\StringUtility::boolToString( $folder2->isAncestorOf( $folder1) ), BR;</example>
+<return-type></return-type>
+<exception>WrongAssetTypeException</exception>
+</documentation>
+*/
+    public function isAncestorOf( Asset $asset ) : bool
     {
         if( !is_subclass_of( $asset, "cascade_ws_asset\ContainedAsset" ) )
             throw new e\WrongAssetTypeException( 
@@ -131,7 +203,14 @@ abstract class Container extends ContainedAsset
             return false;
     }
             
-    public function isParentOf( Asset $asset )
+/**
+<documentation><description><p>Returns a bool, indicating whether this container is the parent container of the named asset.</p></description>
+<example>echo u\StringUtility::boolToString( $folder2->isParentOf( $folder1) ), BR;</example>
+<return-type></return-type>
+<exception>WrongAssetTypeException</exception>
+</documentation>
+*/
+    public function isParentOf( Asset $asset ) : bool
     {
         if( !is_subclass_of( $asset, "cascade_ws_asset\ContainedAsset" ) )
             throw new e\WrongAssetTypeException( 
@@ -140,7 +219,15 @@ abstract class Container extends ContainedAsset
         return $asset->isInContainer( $this );
     }
             
-    public function toChild()
+/**
+<documentation><description><p>Turns the <code>Container</code> object into a <code>p\Child</code> object and returns it.
+This method is used by the <a href="http://www.upstate.edu/cascade-admin/web-services/api/asset-tree/index.php"><code>AssetTree</code></a> class.</p></description>
+<example>u\DebugUtility::dump( $folder2->toChild() );</example>
+<return-type>Child</return-type>
+<exception></exception>
+</documentation>
+*/
+    public function toChild() : p\Child
     {
         $child_std       = new \stdClass();
         $child_std->id   = $this->getId();
