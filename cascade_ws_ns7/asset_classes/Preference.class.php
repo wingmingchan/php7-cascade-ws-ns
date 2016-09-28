c59932095a78fba99ad14cf7cf84df9ccd28a32a
Wing Ming Chan <chanw@upstate.edu>
2016-09-28 09:49:30 -0400

    Updated

--- cascade_ws_ns7/asset_classes/Preference.class.php
+++ cascade_ws_ns7/asset_classes/Preference.class.php
@@ -17,15 +17,18 @@ use cascade_ws_property as p;
 /**
 <documentation>
 <description><h2>Introduction</h2>
-
+<p>A <code>Preference</code> object encapsulates system preferences. This class can be used to work with system preferences, including both reading and editing them. This class is an independent class that does not extend another class.</p>
+<p>As of Cascade 8, there are 62 entries in sytem preferences. Each entry is a key-value pair. To access an entry, we need to supply the key. To edit an entry, we need to supply a key-value pair.</p>
+<p>Note that neither the keys nor the possible values are defined in the WSDL. Therefore, when editing an entry, any value can be attached to an existing key, and Cascade will accept the value. How a meaningless value is treated in the Cascade back-end depends on default values assigned to these keys. When a meaningless value is assigned, Cascade falls back to the default.</p>
+<p>When this class is implemented, I only check the keys. That is to say, when reading or editing an entry, the input key must exist. But I do not check the values for editing. Garbage in, garbage out.</p>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/preference.php">preference.php</a></li></ul></postscript>
 </documentation>
 */
 class Preference
 {
-    const DEBUG = true;
-    const DUMP  = true;
+    const DEBUG = false;
+    const DUMP  = false;
     
     const ALLOW_FONT_ASSIGNMENT                      = "system_pref_allow_font_assignment"; // on, off
     // Label : Content Formatting
@@ -36,8 +39,6 @@ class Preference
     const AVAILABLE_WORKFLOW_TRIGGERS                = "system_pref_available_workflow_triggers";
     const CHECK_EXTERNAL_LINKS                       = "system_pref_check_external_links"; // on, off
     const EXTERNAL_LINKS_TIMEOUT                     = "system_pref_check_external_links_timeout";
-    const CHOOSER_HEIGHT                             = "system_pref_chooser_height";
-    const CHOOSER_WIDTH                              = "system_pref_chooser_width";
     const CSS_CLASSES                                = "system_pref_css_classes";
     const DEFAULT_508_COMPLIANCE_BEHAVIOR            = "system_pref_default_508_compliance_behavior"; // on, off
     const DEFAULT_LANGUAGE                           = "system_pref_default_language";
@@ -58,15 +59,12 @@ class Preference
     const MAX_UPLOAD_IN_KB                           = "system_pref_max_upload_in_kb";
     const OPTIMIZE_OFFICE_HTML                       = "system_pref_optimize_office_html"; // on, off
     const ORGANIZATION_NAME                          = "system_pref_organization_name";
-    const PLAIN_TEXT_FILE_EXTENSIONS                 = "system_pref_plain_text_file_extensions";
     const PUBLISHER_CONCURRENCY                      = "system_pref_publisher_concurrency";
     const PUBLISHER_JOB_TIMEOUT                      = "system_pref_publisher_job_timeout";
     const REMOVE_FONT_AND_STYLE                      = "system_pref_remove_font_and_style"; // on, off
     const REPORT_RELATIVE_LINKS_AS_ERRORS            = "system_pref_report_relative_links_as_errors"; // on, off
     const SEARCH_INDEX_LOCATION                      = "system_pref_search_indexlocation"; // indexes
-    const SEARCH_RESULTS_PER_TYPE                    = "system_pref_search_results_per_type";
     const SESSION_TIMEOUT                            = "system_pref_session_timeout";
-    const SIMPLE_INTERFACE_STARTING_PAGE             = "system_pref_simple_interface_starting_page";
     const SITEWIDE_CSS_FILE                          = "system_pref_sitewide_css_file"; // ID
     const SMTP_EMAIL_ADDRESS                         = "system_pref_smtp_email_address";
     const SMTP_HOST                                  = "system_pref_smtp_host";
@@ -75,7 +73,7 @@ class Preference
     const SMTP_USERNAME                              = "system_pref_smtp_username";
     const SYSTEM_KEYWORDS                            = "system_pref_system_keywords";
     const SYSTEM_URL                                 = "system_pref_system_url";
-    const ULTRA_SIMPLE_INTERFACE                     = "system_pref_ultra_simple_interface"; // on, off
+    const SYSTEM_PREF_SMTPS                          = "system_pref_smtps";
     const XALAN_JAVA_EXTENSIONS                      = "system_pref_xalan_java_extensions"; // on, off
     const INDEX_BLOCK_RENDERING_CACHE                = "system_pref_index_block_rendering_cache"; // on, off
     const MAX_INDEX_BLOCK_SIZE                       = "system_pref_max_index_block_size";
@@ -87,14 +85,14 @@ class Preference
     const LINK_CHECKER_FREQUENCY_DAYS                = "system_pref_link_checker_frequency_days";
     const LINK_CHECKER_FREQUENCY_TIME                = "system_pref_link_checker_frequency_time";
     const GLOBAL_AREA_LINK_CHECKER_ENABLED           = "system_pref_global_area_link_checker_enabled"; // on, off
+    const GLOBAL_AREA_EXTERNAL_LINK_CHECK_ON_PUBLISH = "system_pref_global_area_external_link_check_on_publish";
     const EDITABLE_IMAGE_FILE_EXTENSIONS             = "system_pref_editable_image_file_extensions";
     const SITEWIDE_CSS_FILE_TEXT                     = "system_pref_sitewide_css_fileText";
     const ALLOW_TABLE_EDITING                        = "system_pref_allow_table_editing";
     const TEMPLATE_CREATE_BLOCK_FOLDER               = "system_pref_template_create_block_folder";
     const TEMPLATE_CREATE_BLOCK_FOLDER_TEXT          = "system_pref_template_create_block_folderText";
-    const GLOBAL_AREA_EXTERNAL_LINK_CHECK_ON_PUBLISH = "system_pref_global_area_external_link_check_on_publish";
-    const LIST_SIZE                                  = "system_pref_list_size";
     const UNPUBLISH_BY_DEFAULT_ON_DELETE             = "system_pref_unpublish_by_default_on_delete";
+    const PLAIN_TEXT_FILE_EXTENSIONS                 = "system_pref_plain_text_file_extensions";
 
     const ALLOW_FONT_ASSIGNMENT_MSG                      = "Allow font assignment";
     const ALLOW_FONT_FORMATTING_MSG                      = "Allow text formatting";
@@ -104,8 +102,6 @@ class Preference
     const AVAILABLE_WORKFLOW_TRIGGERS_MSG                = "Available workflow triggers";
     const CHECK_EXTERNAL_LINKS_MSG                       = "Check external links";
     const EXTERNAL_LINKS_TIMEOUT_MSG                     = "Check external links timeout";
-    const CHOOSER_HEIGHT_MSG                             = "Chooser height";
-    const CHOOSER_WIDTH_MSG                              = "Chooser width";
     const CSS_CLASSES_MSG                                = "CSS classes";
     const DEFAULT_508_COMPLIANCE_BEHAVIOR_MSG            = "Default 508 compliance behavior";
     const DEFAULT_LANGUAGE_MSG                           = "Default language";
@@ -126,15 +122,14 @@ class Preference
     const MAX_UPLOAD_IN_KB_MSG                           = "Max upload in kb";
     const OPTIMIZE_OFFICE_HTML_MSG                       = "Optimize office HTML";
     const ORGANIZATION_NAME_MSG                          = "Organization name";
-    const PLAIN_TEXT_FILE_EXTENSIONS_MSG                 = "Plain text file extensions";
     const PUBLISHER_CONCURRENCY_MSG                      = "Publisher concurrency";
     const PUBLISHER_JOB_TIMEOUT_MSG                      = "Publisher job timeout";
     const REMOVE_FONT_AND_STYLE_MSG                      = "Remove font and style";
     const REPORT_RELATIVE_LINKS_AS_ERRORS_MSG            = "Report relative links as errors";
     const SEARCH_INDEX_LOCATION_MSG                      = "Search indexlocation";
-    const SEARCH_RESULTS_PER_TYPE_MSG                    = "Search results per type";
+    //const SEARCH_RESULTS_PER_TYPE_MSG                    = "Search results per type";
     const SESSION_TIMEOUT_MSG                            = "Session timeout";
-    const SIMPLE_INTERFACE_STARTING_PAGE_MSG             = "Simple interface starting page";
+    //const SIMPLE_INTERFACE_STARTING_PAGE_MSG             = "Simple interface starting page";
     const SITEWIDE_CSS_FILE_MSG                          = "Sitewide CSS file";
     const SMTP_EMAIL_ADDRESS_MSG                         = "SMTP email address";
     const SMTP_HOST_MSG                                  = "SMTP host";
@@ -143,7 +138,8 @@ class Preference
     const SMTP_USERNAME_MSG                              = "SMTP username";
     const SYSTEM_KEYWORDS_MSG                            = "System keywords";
     const SYSTEM_URL_MSG                                 = "System URL";
-    const ULTRA_SIMPLE_INTERFACE_MSG                     = "Ultra simple interface";
+    const SYSTEM_PREF_SMTPS_MSG                          = "System pref smtps";
+    //const ULTRA_SIMPLE_INTERFACE_MSG                     = "Ultra simple interface";
     const XALAN_JAVA_EXTENSIONS_MSG                      = "Xalan java extensions";
     const INDEX_BLOCK_RENDERING_CACHE_MSG                = "Index block rendering cache";
     const MAX_INDEX_BLOCK_SIZE_MSG                       = "Max index block size";
@@ -155,14 +151,14 @@ class Preference
     const LINK_CHECKER_FREQUENCY_DAYS_MSG                = "Link checker frequency days";
     const LINK_CHECKER_FREQUENCY_TIME_MSG                = "Link checker frequency time";
     const GLOBAL_AREA_LINK_CHECKER_ENABLED_MSG           = "Global area link checker enabled";
+    const GLOBAL_AREA_EXTERNAL_LINK_CHECK_ON_PUBLISH_MSG = "Global area external link check on publish";
     const EDITABLE_IMAGE_FILE_EXTENSIONS_MSG             = "Editable image file extensions";
     const SITEWIDE_CSS_FILE_TEXT_MSG                     = "Sitewide css file text";
     const ALLOW_TABLE_EDITING_MSG                        = "Allow table editing";
     const TEMPLATE_CREATE_BLOCK_FOLDER_MSG               = "Template create block folder";
     const TEMPLATE_CREATE_BLOCK_FOLDER_TEXT_MSG          = "Template create block folder text";
-    const GLOBAL_AREA_EXTERNAL_LINK_CHECK_ON_PUBLISH_MSG = "Global area external link check on publish";
-    const LIST_SIZE_MSG                                  = "List size";
     const UNPUBLISH_BY_DEFAULT_ON_DELETE_MSG             = "Unpublish by default on delete";
+    const PLAIN_TEXT_FILE_EXTENSIONS_MSG                 = "Plain text file extensions";
 
 /**
 <documentation><description><p></p></description>
@@ -199,12 +195,12 @@ class Preference
     
 /**
 <documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<example>$cascade->getPreference()->display();</example>
+<return-type>Preference</return-type>
 <exception></exception>
 </documentation>
 */
-    public function display()
+    public function display() : Preference
     {
         echo "<table  class='preferences' summary='Preferences'>" .
             "<caption>System Preferences</caption>" .
@@ -222,7 +218,7 @@ class Preference
             "<tr><td>" .  self::ASSET_TREE_MODE_MSG .
             "</td><td>" . $this->map[ self::ASSET_TREE_MODE ] . "</td></tr>" .
             
-            "<tr><td>" .  self::ASSET_TREE_MODE_MSG .
+            "<tr><td>" .  self::AVAILABLE_ASST_FACTORY_PLUGINS .
             "</td><td>" . $this->map[ self::AVAILABLE_ASST_FACTORY_PLUGINS ] . "</td></tr>" .
             
             "<tr><td>" .  self::AVAILABLE_WORKFLOW_TRIGGERS_MSG .
@@ -233,12 +229,6 @@ class Preference
             
             "<tr><td>" .  self::EXTERNAL_LINKS_TIMEOUT_MSG .
             "</td><td>" . $this->map[ self::EXTERNAL_LINKS_TIMEOUT ] . "</td></tr>" .
-            
-            "<tr><td>" .  self::CHOOSER_HEIGHT_MSG .
-            "</td><td>" . $this->map[ self::CHOOSER_HEIGHT ] . "</td></tr>" .
-            
-            "<tr><td>" .  self::CHOOSER_WIDTH_MSG .
-            "</td><td>" . $this->map[ self::CHOOSER_WIDTH ] . "</td></tr>" .
             
             "<tr><td>" .  self::CSS_CLASSES_MSG .
             "</td><td>" . $this->map[ self::CSS_CLASSES ] . "</td></tr>" .
@@ -297,17 +287,8 @@ class Preference
             "<tr><td>" .  self::OPTIMIZE_OFFICE_HTML_MSG .
             "</td><td>" . $this->map[ self::OPTIMIZE_OFFICE_HTML ] . "</td></tr>" .
             
-            "<tr><td>" .  self::OPTIMIZE_OFFICE_HTML_MSG .
-            "</td><td>" . $this->map[ self::OPTIMIZE_OFFICE_HTML ] . "</td></tr>" .
-            
-            "<tr><td>" .  self::OPTIMIZE_OFFICE_HTML_MSG .
-            "</td><td>" . $this->map[ self::OPTIMIZE_OFFICE_HTML ] . "</td></tr>" .
-            
             "<tr><td>" .  self::ORGANIZATION_NAME_MSG .
             "</td><td>" . $this->map[ self::ORGANIZATION_NAME ] . "</td></tr>" .
-            
-            "<tr><td>" .  self::PLAIN_TEXT_FILE_EXTENSIONS_MSG .
-            "</td><td>" . $this->map[ self::PLAIN_TEXT_FILE_EXTENSIONS ] . "</td></tr>" .
             
             "<tr><td>" .  self::PUBLISHER_CONCURRENCY_MSG .
             "</td><td>" . $this->map[ self::PUBLISHER_CONCURRENCY ] . "</td></tr>" .
@@ -324,14 +305,8 @@ class Preference
             "<tr><td>" .  self::SEARCH_INDEX_LOCATION_MSG .
             "</td><td>" . $this->map[ self::SEARCH_INDEX_LOCATION ] . "</td></tr>" .
             
-            "<tr><td>" .  self::SEARCH_RESULTS_PER_TYPE_MSG .
-            "</td><td>" . $this->map[ self::SEARCH_RESULTS_PER_TYPE ] . "</td></tr>" .
-            
             "<tr><td>" .  self::SESSION_TIMEOUT_MSG .
             "</td><td>" . $this->map[ self::SESSION_TIMEOUT ] . "</td></tr>" .
-            
-            "<tr><td>" .  self::SIMPLE_INTERFACE_STARTING_PAGE_MSG .
-            "</td><td>" . $this->map[ self::SIMPLE_INTERFACE_STARTING_PAGE ] . "</td></tr>" .
             
             "<tr><td>" .  self::SITEWIDE_CSS_FILE_MSG .
             "</td><td>" . $this->map[ self::SITEWIDE_CSS_FILE ] . "</td></tr>" .
@@ -357,8 +332,8 @@ class Preference
             "<tr><td>" .  self::SYSTEM_URL_MSG .
             "</td><td>" . $this->map[ self::SYSTEM_URL ] . "</td></tr>" .
             
-            "<tr><td>" .  self::ULTRA_SIMPLE_INTERFACE_MSG .
-            "</td><td>" . $this->map[ self::ULTRA_SIMPLE_INTERFACE ] . "</td></tr>" .
+            "<tr><td>" .  self::SYSTEM_PREF_SMTPS_MSG .
+            "</td><td>" . $this->map[ self::SYSTEM_PREF_SMTPS ] . "</td></tr>" .
             
             "<tr><td>" .  self::XALAN_JAVA_EXTENSIONS_MSG .
             "</td><td>" . $this->map[ self::XALAN_JAVA_EXTENSIONS ] . "</td></tr>" .
@@ -393,6 +368,9 @@ class Preference
             "<tr><td>" .  self::GLOBAL_AREA_LINK_CHECKER_ENABLED_MSG .
             "</td><td>" . $this->map[ self::GLOBAL_AREA_LINK_CHECKER_ENABLED ] . "</td></tr>" .
             
+            "<tr><td>" .  self::GLOBAL_AREA_EXTERNAL_LINK_CHECK_ON_PUBLISH_MSG .
+            "</td><td>" . $this->map[ self::GLOBAL_AREA_EXTERNAL_LINK_CHECK_ON_PUBLISH ] . "</td></tr>" .
+            
             "<tr><td>" .  self::EDITABLE_IMAGE_FILE_EXTENSIONS_MSG .
             "</td><td>" . $this->map[ self::EDITABLE_IMAGE_FILE_EXTENSIONS ] . "</td></tr>" .
             
@@ -405,14 +383,15 @@ class Preference
             "<tr><td>" .  self::TEMPLATE_CREATE_BLOCK_FOLDER_MSG .
             "</td><td>" . $this->map[ self::TEMPLATE_CREATE_BLOCK_FOLDER ] . "</td></tr>" .
             
-            "<tr><td>" .  self::GLOBAL_AREA_EXTERNAL_LINK_CHECK_ON_PUBLISH_MSG .
-            "</td><td>" . $this->map[ self::GLOBAL_AREA_EXTERNAL_LINK_CHECK_ON_PUBLISH ] . "</td></tr>" .
-            
-            "<tr><td>" .  self::LIST_SIZE_MSG .
-            "</td><td>" . $this->map[ self::LIST_SIZE ] . "</td></tr>" .
+            "<tr><td>" .  self::TEMPLATE_CREATE_BLOCK_FOLDER_TEXT_MSG .
+            "</td><td>" . $this->map[ self::TEMPLATE_CREATE_BLOCK_FOLDER_TEXT ] . "</td></tr>" .
             
             "<tr><td>" .  self::UNPUBLISH_BY_DEFAULT_ON_DELETE_MSG .
             "</td><td>" . $this->map[ self::UNPUBLISH_BY_DEFAULT_ON_DELETE ] . "</td></tr>" .
+            
+            
+            "<tr><td>" .  self::PLAIN_TEXT_FILE_EXTENSIONS_MSG .
+            "</td><td>" . $this->map[ self::PLAIN_TEXT_FILE_EXTENSIONS ] . "</td></tr>" .
             
             "<tr><td>" .  "A_dummy_string_to_push_the_content_to_the_right" .
             "</td><td>" . "" . "</td></tr>" .
@@ -423,12 +402,12 @@ class Preference
     
 /**
 <documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<example>$cascade->getPreference()->dump();</example>
+<return-type>Preference</return-type>
 <exception></exception>
 </documentation>
 */
-    public function dump( $formatted=false )
+    public function dump( bool $formatted=true ) : Preference
     {
         if( $formatted ) echo S_H2 . c\L::READ_DUMP . E_H2 . S_PRE;
         var_dump( $this->preference_std );
@@ -439,12 +418,12 @@ class Preference
 
 /**
 <documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<example>echo $pref->getValue( a\Preference::ASSET_TREE_MODE );</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getValue( $name )
+    public function getValue( string $name ) : string
     {
         if( !in_array( $name, $this->names ) )
             throw new e\NoSuchNameException( "The name $name does not exist. " );
@@ -454,24 +433,24 @@ class Preference
   
 /**
 <documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<example>u\DebugUtility::dump( $pref->toStdClass() );</example>
+<return-type>stdClass</return-type>
 <exception></exception>
 </documentation>
 */
-    public function toStdClass()
+    public function toStdClass() : \stdClass
     {
         return $this->preference_std;
     }
     
 /**
 <documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<example>$pref->setValue( a\Preference::ALLOW_TABLE_EDITING, "on" );</example>
+<return-type>Preference</return-type>
 <exception></exception>
 </documentation>
 */
-    public function setValue( $name, $value )
+    public function setValue( string $name, string $value ) : Preference
     {
         if( !in_array( $name, $this->names ) )
             throw new e\NoSuchNameException( "The name $name does not exist. " );
