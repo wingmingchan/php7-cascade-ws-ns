ed4f262bee3e20375ccc6dd6cf65bcb08238b8d8
Wing Ming Chan <chanw@upstate.edu>
2016-10-17 09:03:56 -0400

    Updated

--- cascade_ws_ns7/asset_classes/ScriptFormat.class.php
+++ cascade_ws_ns7/asset_classes/ScriptFormat.class.php
@@ -9,17 +9,32 @@
 namespace cascade_ws_asset;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
-use cascade_ws_property as p;
+use cascade_ws_property  as p;
 
 /**
 <documentation>
 <description><h2>Introduction</h2>
-
+<p>A <code>ScriptFormat</code> represents a script format asset. The class <code>ScriptFormat</code> is a sub-class of <a href="http://www.upstate.edu/cascade-admin/web-services/api/asset-classes/format.php"><code>Format</code></a>.</p>
+<h2>Structure of <code>scriptFormat</code></h2>
+<pre>scriptFormat
+  id
+  name
+  parentFolderId
+  parentFolderPath
+  path
+  lastModifiedDate
+  lastModifiedBy
+  createdDate
+  createdBy
+  siteId
+  siteName
+  script
+</pre>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/script_format.php">script_format.php</a></li></ul></postscript>
 </documentation>
 */
 class ScriptFormat extends Format
@@ -28,13 +43,13 @@ class ScriptFormat extends Format
     const TYPE  = c\T::VELOCITYFORMAT;
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Displays the script and returns the calling object.</p></description>
+<example>$f->displayScript();</example>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function displayScript()
+    public function displayScript() : Asset
     {
         $script_string = htmlentities( $this->getProperty()->script ); // &
         $script_string = u\XMLUtility::replaceBrackets( $script_string );
@@ -46,25 +61,25 @@ class ScriptFormat extends Format
     }
 
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>script</code>.</p></description>
+<example>echo $f->getScript(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getScript()
+    public function getScript() : string
     {
         return $this->getProperty()->script;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Sets <code>script</code> and returns the calling object.</p></description>
+<example>$f->setScript( $script )->edit();</example>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function setScript( $script )
+    public function setScript( $script ) : Asset
     {
         $this->getProperty()->script = $script;
         
