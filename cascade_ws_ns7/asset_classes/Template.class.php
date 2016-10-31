25ff387836b1b84d1a99944442576d960befdf2b
Wing Ming Chan <chanw@upstate.edu>
2016-10-31 13:38:02 -0400

    Updated

--- cascade_ws_ns7/asset_classes/Template.class.php
+++ cascade_ws_ns7/asset_classes/Template.class.php
@@ -12,17 +12,53 @@
 namespace cascade_ws_asset;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
-use cascade_ws_property as p;
+use cascade_ws_property  as p;
 
 /**
 <documentation>
 <description><h2>Introduction</h2>
-
+<p>A <code>Template</code> object represents a template asset. It contains <a href="http://www.upstate.edu/cascade-admin/web-services/api/property-classes/page-region.php"><code>PageRegion</code></a> objects and can be associated with an <a href="http://www.upstate.edu/cascade-admin/web-services/api/asset-classes/xslt-format.php"><code>XsltFormat</code></a> object.</p>
+<h2>Structure of <code>template</code></h2>
+<pre>template
+  id
+  name
+  parentFolderId
+  parentFolderPath
+  path
+  lastModifiedDate
+  lastModifiedBy
+  createdDate
+  createdBy
+  siteId
+  siteName
+  targetId
+  targetPath
+  formatId
+  formatPath
+  formatRecycled
+  xml
+  pageRegions
+    pageRegion (NULL, stdClass or array of stdClass)
+      id
+      name
+      blockId
+      blockPath
+      blockRecycled
+      noBlock
+      formatId
+      formatPath
+      formatRecycled
+      noFormat
+</pre>
+<h2>Design Issues</h2>
+<ul>
+<li>There is no <code>set</code> method to work with targets.</li>
+</ul>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/template.php">template.php</a></li></ul></postscript>
 </documentation>
 */
 class Template extends ContainedAsset
@@ -31,7 +67,7 @@ class Template extends ContainedAsset
     const TYPE  = c\T::TEMPLATE;
     
 /**
-<documentation><description><p></p></description>
+<documentation><description><p>The constructor, overriding the parent method to process page regions.</p></description>
 <example></example>
 <return-type></return-type>
 <exception></exception>
@@ -54,13 +90,14 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Displays <code>xml</code> and returns the calling
+object.</p></description>
+<example>$t->displayXml();</example>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function displayXml()
+    public function displayXml() : Asset
     {
         $xml_string = u\XMLUtility::replaceBrackets( $this->xml );
         
@@ -71,7 +108,7 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
+<documentation><description><p>Edits and returns the calling object.</p></description>
 <example></example>
 <return-type></return-type>
 <exception></exception>
@@ -111,33 +148,33 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>createdBy</code>.</p></description>
+<example>echo $t->getCreatedBy(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getCreatedBy()
+    public function getCreatedBy() : string
     {
         return $this->getProperty()->createdBy;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>createdDate</code>.</p></description>
+<example>echo $t->getCreatedDate(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getCreatedDate()
+    public function getCreatedDate() : string
     {
         return $this->getProperty()->createdDate;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>NULL</code> or an <code>XsltFormat</code> object.</p></description>
+<example>$f = $t->getFormat();</example>
+<return-type>mixed</return-type>
 <exception></exception>
 </documentation>
 */
@@ -154,9 +191,9 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>formatId</code> of the template.</p></description>
+<example>echo u\StringUtility::getCoalescedString( $t->getFormatId() ), BR;</example>
+<return-type>mixed</return-type>
 <exception></exception>
 </documentation>
 */
@@ -166,9 +203,9 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>formatPath</code> of the template.</p></description>
+<example>echo u\StringUtility::getCoalescedString( $t->getFormatPath() ), BR;</example>
+<return-type>mixed</return-type>
 <exception></exception>
 </documentation>
 */
@@ -178,49 +215,49 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>formatRecycled</code>.</p></description>
+<example>echo u\StringUtility::boolToString( $t->getFormatRecycled() ), BR;</example>
+<return-type>bool</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getFormatRecycled()
+    public function getFormatRecycled() : bool
     {
         return $this->getProperty()->formatRecycled;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>lastModifiedBy</code>.</p></description>
+<example>echo $t->getLastModifiedBy(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getLastModifiedBy()
+    public function getLastModifiedBy() : string
     {
         return $this->getProperty()->lastModifiedBy;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>lastModifiedDate</code>.</p></description>
+<example>echo $t->getLastModifiedDate(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getLastModifiedDate()
+    public function getLastModifiedDate() : string
     {
         return $this->getProperty()->lastModifiedDate;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the <code>PageRegion</code> object bearing the name.</p></description>
+<example>u\DebugUtility::dump( $t->getPageRegion( 'STORAGE' ) );</example>
+<return-type>Property</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getPageRegion( $name )
+    public function getPageRegion( string $name ) : p\Property
     {
         if( self::DEBUG ) { u\DebugUtility::dump( $this->page_region_map ); }
         
@@ -232,44 +269,44 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>Returns the <code>Block</code> object of the region, or <code>NULL</code>.</p></description>
+<example>$block = $t->getPageRegionBlock( 'STORAGE' );</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
 */
-    public function getPageRegionBlock( $region_name )
+    public function getPageRegionBlock( string $region_name )
     {
         return $this->getPageRegion( $region_name )->getBlock();
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>Returns the <code>Format</code> object of the region, or <code>NULL</code>.</p></description>
+<example>$format = $t->getPageRegionFormat( 'STORAGE' );</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
 */
-    public function getPageRegionFormat( $region_name )
+    public function getPageRegionFormat( string $region_name )
     {
         return $this->getPageRegion( $region_name )->getFormat();
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns an array of page region names.</p></description>
+<example>u\DebugUtility::dump( $t->getPageRegionNames() );</example>
+<return-type>array</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getPageRegionNames()
+    public function getPageRegionNames() : array
     {
         return array_keys( $this->page_region_map );
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>Returns the array of <code>p\PageRegion</code> objects.</p></description>
+<example>u\DebugUtility::dump( $t->getPageRegions() );</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
@@ -280,8 +317,8 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>Returns an <code>stdClass</code> object representing all page regions.</p></description>
+<example>u\DebugUtility::dump( $t->getPageRegionStdForPageConfiguration() );</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
@@ -301,18 +338,20 @@ class Template extends ContainedAsset
         }
         
         $std          = new \stdClass();
-         $region_count = count( $temp );
+        $region_count = count( $temp );
        
-           if( $region_count == 0 )
-           {
-               // do nothing
-           }
+        if( $region_count == 0 )
+        {
+            // do nothing
+        }
         else if( $region_count == 1 )
         {
+            $std->pageRegions = new \stdClass();
             $std->pageRegions->pageRegion = $temp[ 0 ]->toStdClass();
         }
         else
         {
+            $std->pageRegions = new \stdClass();
             $std->pageRegions->pageRegion = array();
             
             for( $i = 0; $i < $region_count; $i++ )
@@ -325,7 +364,7 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
+<documentation><description><p>An alias of <code>getPageRegionNames()</code>.</p></description>
 <example></example>
 <return-type></return-type>
 <exception></exception>
@@ -337,9 +376,9 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
+<documentation><description><p>Returns <code>targetId</code>.</p></description>
 <example></example>
-<return-type></return-type>
+<return-type>mixed</return-type>
 <exception></exception>
 </documentation>
 */
@@ -349,9 +388,9 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
+<documentation><description><p>Returns <code>targetPath</code>.</p></description>
 <example></example>
-<return-type></return-type>
+<return-type>mixed</return-type>
 <exception></exception>
 </documentation>
 */
@@ -361,37 +400,37 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>xml</code>.</p></description>
+<example>echo $t->getXml(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getXml()
+    public function getXml() : string
     {
         return $this->xml;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns a bool, indicating whether the <code>p\PageRegion</code> bearing that name exists.</p></description>
+<example>echo u\StringUtility::boolToString( $t->hasPageRegion( 'STORAGE' ) ), BR;</example>
+<return-type>bool</return-type>
 <exception></exception>
 </documentation>
 */
-    public function hasPageRegion( $name )
+    public function hasPageRegion( string $name ) : bool
     {
         return isset( $this->page_region_map[ $name ] );
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Sets the format, and returns the calling object.</p></description>
+<example>$t->setFormat( $format )->edit();</example>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function setFormat( Format $format=NULL )
+    public function setFormat( Format $format=NULL ) : Asset
     {
         if( isset( $format ) )
         {
@@ -414,13 +453,14 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
+<documentation><description><p>Replaces the old <code>p\PageRegion</code> object with the new one, and returns the calling object.</p></description>
 <example></example>
-<return-type></return-type>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function setPageRegion( $name, p\PageRegion $page_region )
+    public function setPageRegion(
+        string $name, p\PageRegion $page_region ) : Asset
     {
         if( !isset( $this->page_region_map[ $name ] ) )
         {
@@ -446,13 +486,18 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Attaches the block to the named page region, and returns
+the calling object.</p></description>
+<example>$t->setPageRegionBlock( 'DEFAULT', NULL )->
+    setPageRegionFormat( 'DEFAULT', NULL )->
+    edit()->dump();</example>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function setPageRegionBlock( $name, $block=NULL, $block_recycled=false, $no_block=false )
+    public function setPageRegionBlock( 
+        string $name, Block $block=NULL, 
+        bool $block_recycled=false, bool $no_block=false ) : Asset
     {
         $page_region = $this->getPageRegion( $name );
         $page_region->setBlock( $block, $block_recycled, $no_block );
@@ -462,13 +507,18 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Attaches the format to the named page region, and returns
+the calling object.</p></description>
+<example>$t->setPageRegionBlock( 'DEFAULT', NULL )->
+    setPageRegionFormat( 'DEFAULT', NULL )->
+    edit()->dump();</example>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function setPageRegionFormat( $name, $format=NULL, $format_recycled=false, $no_format=false )
+    public function setPageRegionFormat(
+        string $name, Format $format=NULL,
+        bool $format_recycled=false, bool $no_format=false ) : Asset
     {
         $page_region = $this->getPageRegion( $name );
         $page_region->setFormat( $format, $format_recycled, $no_format );
@@ -478,13 +528,14 @@ class Template extends ContainedAsset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Sets <code>xml</code> and returns the calling
+object.</p></description>
+<example>$t->setXML( $xml )->edit();</example>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function setXml( $xml )
+    public function setXml( string $xml ) : Asset
     {
         if( trim( $xml ) == "" )
             throw new e\EmptyValueException( 
@@ -494,8 +545,16 @@ class Template extends ContainedAsset
         return $this;
     }
     
+/**
+<documentation><description><p>Processes page regions, used by this class and <a href="http://www.upstate.edu/cascade-admin/web-services/api/property-classes/page-configuration.php"><code>p\PageConfiguration</code></a>.</p></description>
+<example></example>
+<return-type>Asset</return-type>
+<exception></exception>
+</documentation>
+*/
     public static function processPageRegions( 
-        $regions, &$page_regions, &$page_region_map, $service )
+        $regions, array &$page_regions, &$page_region_map, 
+        aohs\AssetOperationHandlerService $service )
     {
         if( $regions == NULL )
             return;
