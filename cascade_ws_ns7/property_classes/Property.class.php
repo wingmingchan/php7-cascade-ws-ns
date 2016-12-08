a52abdb15b4ef788a9af8c99e990775bc56c02ec
Wing Ming Chan <chanw@upstate.edu>
2016-12-08 15:05:38 -0500

    Updated

--- cascade_ws_ns7/property_classes/Property.class.php
+++ cascade_ws_ns7/property_classes/Property.class.php
@@ -15,15 +15,40 @@ use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 /**
- * Property
- * The ancestor class of all property classes.
- *
- * @link http://www.upstate.edu/cascade-admin/projects/web-services/oop/classes/property-classes/property.php
- */
+<documentation><description>
+<h2>Relationship between <code>Property</code> and Other Property Classes</h2>
+<p>I decide to make <code>Property</code> an abstract class. Other property classes like <code>PageRegion</code> and <code>Metadata</code> should extend this abstract class. Later, we may need interfaces of which the <code>Property</code> class will be an implementation.</p>
+<h2>Definition</h2>
+<p>The <code>Property</code> class is defined in this way:</p>
+<pre class="code">&lt;?php 
+abstract class Property
+{
+    public abstract function __construct( 
+        \stdClass $obj=NULL, 
+        aohs\AssetOperationHandlerService $service=NULL, 
+        $data1=NULL, 
+        $data2=NULL, 
+        $data3=NULL );
+
+    public abstract function toStdClass();
+}
+?&gt;
+</pre>
+<p>The constructor converts an <code>\stdClass</code> object to a <code>Property</code> object, and the <code>toStdClass</code> method convert a <code>Property</code> object back to an <code>\stdClass</code> object. Parameters like <code>$data1</code> are used by individual sub-classes.</p>
+</description>
+</documentation>
+*/
 abstract class Property
 {
 	const NAME_SPACE = "cascade_ws_property";
 
+/**
+<documentation><description></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public abstract function __construct(
         \stdClass $obj=NULL, 
         aohs\AssetOperationHandlerService $service=NULL, 
@@ -31,6 +56,13 @@ abstract class Property
         $data2=NULL, 
         $data3=NULL );
         
+/**
+<documentation><description></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public abstract function toStdClass();
 }
 ?>
