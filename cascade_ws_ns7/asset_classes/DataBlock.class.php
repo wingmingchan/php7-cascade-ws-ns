ced2b658357eabcbcc4995f38fa4393a245b76c2
Wing Ming Chan <chanw@upstate.edu>
2016-12-14 13:11:24 -0500

    Updated

--- cascade_ws_ns7/asset_classes/DataBlock.class.php
+++ cascade_ws_ns7/asset_classes/DataBlock.class.php
@@ -17,9 +17,11 @@ use cascade_ws_property as p;
 /**
 <documentation>
 <description><h2>Introduction</h2>
-
+<p>While I am arguing that there is no need to split the class <a href="/web-services/api/asset-classes/data-definition-block"><code>DataDefinitionBlock</code></a> into two to do justice to xhtmlDataDefinition blocks without the support of data definitions, I know that it is unfair to name the class <code>DataDefinitionBlock</code>. For blocks not using data definitions, <code>DataDefinitionBlock</code> is simply a misnomer. To remedy this problem, I want to use alias again. But this time, I want to use a class alias.</p>
+<p>Class aliases are easy to create. We already have a class <code>DataDefinitionBlock</code>. We just need to extend this class and give the new class a new name. There is no implementation in the new class. It just inherits everything from its parent. In the test code, we simply change the old class name to the new class name. Everything works fine.</p>
+<p><code>DataBlock</code> is just an alias (to be exact, an empty sub-class) of <a href="/web-services/api/asset-classes/data-definition-block"><code>DataDefinitionBlock</code></a>. I hope that to a certain extent this will do justice to xhtmlDataDefinition blocks without the support of data definitions.</p>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+<postscript></postscript>
 </documentation>
 */
 class DataBlock extends DataDefinitionBlock
