61168033b33462b220db59d9998dadbe79e0fb2e
Wing Ming Chan <chanw@upstate.edu>
2016-12-14 14:27:07 -0500

    Updated

--- cascade_ws_ns7/asset_classes/SiteDestinationContainer.class.php
+++ cascade_ws_ns7/asset_classes/SiteDestinationContainer.class.php
@@ -9,17 +9,36 @@
 namespace cascade_ws_asset;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
-use cascade_ws_property as p;
+use cascade_ws_property  as p;
 
 /**
 <documentation>
 <description><h2>Introduction</h2>
-
+<p>A <code>SiteDestinationContainer</code> object represents a site destination container asset. This class is a sub-class of <a href="/web-services/api/asset-classes/container"><code>Container</code></a>.</p>
+<h2>Structure of <code>siteDestinationContainer</code></h2>
+<pre>siteDestinationContainer
+  id
+  name
+  parentContainerId
+  parentContainerPath
+  path
+  siteId
+  siteName
+  children
+    child
+      id
+      path
+        path
+        siteId
+        siteName
+      type
+      recycled
+</pre>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/site_destination_container.php">site_destination_container.php</a></li></ul></postscript>
 </documentation>
 */
 class SiteDestinationContainer extends Container
