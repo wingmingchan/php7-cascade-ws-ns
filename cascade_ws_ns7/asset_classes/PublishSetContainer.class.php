5f1a5d982f4ab3053e19a8ad726a4b0ef216a4d5
Wing Ming Chan <chanw@upstate.edu>
2016-12-14 14:24:51 -0500

    Updated

--- cascade_ws_ns7/asset_classes/PublishSetContainer.class.php
+++ cascade_ws_ns7/asset_classes/PublishSetContainer.class.php
@@ -17,9 +17,28 @@ use cascade_ws_property  as p;
 /**
 <documentation>
 <description><h2>Introduction</h2>
-
+<p>A <code>PublishSetContainer</code> object represents a publish set container asset. This class is a sub-class of <a href="/web-services/api/asset-classes/container"><code>Container</code></a>.</p>
+<h2>Structure of <code>publishSetContainer</code></h2>
+<pre>publishSetContainer
+  id
+  name
+  parentContainerId
+  parentContainerPath
+  path
+  siteId
+  siteName
+  children
+    child
+      id
+      path
+        path
+        siteId
+        siteName
+      type
+      recycled
+</pre>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/publish_set_container.php">publish_set_container.php</a></li></ul></postscript>
 </documentation>
 */
 class PublishSetContainer extends Container
