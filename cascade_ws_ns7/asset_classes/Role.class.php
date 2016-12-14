4c0bf0a9ab211257f26648d37339cad4251622c0
Wing Ming Chan <chanw@upstate.edu>
2016-12-14 14:26:00 -0500

    Updated

--- cascade_ws_ns7/asset_classes/Role.class.php
+++ cascade_ws_ns7/asset_classes/Role.class.php
@@ -9,17 +9,30 @@
 namespace cascade_ws_asset;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
-use cascade_ws_property as p;
+use cascade_ws_property  as p;
 
 /**
 <documentation>
 <description><h2>Introduction</h2>
-
+<p>A <code>Role</code> object represents a role asset.</p>
+<p>There are two types of roles in Cascade: global and site. In a <code>role</code> property, there are two sub-properties: <code>globalAbilities</code> and <code>siteAbilities</code>. For a global role, the <code>siteAbilities</code> property stores a <code>NULL</code> value. For a site role, the <code>globalAbilities</code> property stores a <code>NULL</code> value. Corresponding to these two properties, there are two classes: <a href="site://cascade-admin-old/projects/web-services/oop/classes/property-classes/global-abilities"><code>p\GlobalAbilities</code></a> and <a href="site://cascade-admin-old/projects/web-services/oop/classes/property-classes/site-abilities"><code>p\SiteAbilities</code></a>. These two classes are sub-classes of the <a href="site://cascade-admin-old/projects/web-services/oop/classes/property-classes/abilities"><code>p\Abilities</code></a> class. A <code>Role</code> object has a <code>p\GlobalAbilities</code> object and a <code>p\SiteAbilities</code> object.</p>
+<h2>Structure of <code>role</code></h2>
+<pre>role
+  id
+  name
+  roleType
+  globalAbilities
+    (83 properties, v.8)
+  siteAbilities
+    (52 properties)
+</pre>
+<h2>Design Issues</h2>
+<p>Since there are too many methods (85 <code>get</code> and 85 <code>set</code> methods) involved here, I decide not to repeat these methods in various classes. Instead, I provide two <code>get</code> methods, i.e., <code>getGlobalAbilities()</code> and <code>getSiteAbilities()</code> in this class, each returning an <code>Abilities</code> object, allowing us to manipulate these two objects directly. Therefore, there are no <code>set</code> methods in this class.</p>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/role.php">role.php</a></li></ul></postscript>
 </documentation>
 */
 class Role extends Asset
@@ -28,7 +41,7 @@ class Role extends Asset
     const TYPE  = c\T::ROLE;
     
 /**
-<documentation><description><p></p></description>
+<documentation><description><p>The constructor, overriding the parent method to process the abilities.</p></description>
 <example></example>
 <return-type></return-type>
 <exception></exception>
@@ -40,21 +53,23 @@ class Role extends Asset
         parent::__construct( $service, $identifier );
         
         if( isset( $this->getProperty()->globalAbilities ) )
-            $this->global_abilities = new p\GlobalAbilities( $this->getProperty()->globalAbilities );
+            $this->global_abilities = new p\GlobalAbilities(
+                $this->getProperty()->globalAbilities );
         else
             $this->global_abilities = NULL;
             
         if( isset( $this->getProperty()->siteAbilities ) )
-            $this->site_abilities   = new p\SiteAbilities( $this->getProperty()->siteAbilities );
+            $this->site_abilities   = new p\SiteAbilities(
+                $this->getProperty()->siteAbilities );
         else
             $this->site_abilities = NULL;
     }
 
 /**
-<documentation><description><p></p></description>
+<documentation><description><p>Edits and returns the calling object.</p></description>
 <example></example>
-<return-type></return-type>
-<exception></exception>
+<return-type>Asset</return-type>
+<exception>EditingFailureException</exception>
 </documentation>
 */
     public function edit(
@@ -90,9 +105,9 @@ class Role extends Asset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the <code>p\GlobalAbilities</code> object or <code>NULL</code>.</p></description>
+<example>$ga = $r->getGlobalAbilities();</example>
+<return-type>mixed</return-type>
 <exception></exception>
 </documentation>
 */
@@ -102,9 +117,9 @@ class Role extends Asset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>roleType</code>.</p></description>
+<example>echo $r->getRoleType(), BR;</example>
+<return-type>mixed</return-type>
 <exception></exception>
 </documentation>
 */
@@ -114,8 +129,8 @@ class Role extends Asset
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>Returns the <code>p\SiteAbilities</code> object or <code>NULL</code>.</p></description>
+<example>$sa = $r->getSiteAbilities();</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
