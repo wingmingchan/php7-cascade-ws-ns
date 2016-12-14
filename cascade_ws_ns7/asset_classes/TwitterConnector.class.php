ccfea50a0f62a61cf2e7c11dfb647e12d87f42c4
Wing Ming Chan <chanw@upstate.edu>
2016-12-14 14:28:56 -0500

    Updated

--- cascade_ws_ns7/asset_classes/TwitterConnector.class.php
+++ cascade_ws_ns7/asset_classes/TwitterConnector.class.php
@@ -9,17 +9,45 @@
 namespace cascade_ws_asset;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
-use cascade_ws_property as p;
+use cascade_ws_property  as p;
 
 /**
 <documentation>
 <description><h2>Introduction</h2>
-
+<p>A <code>TwitterConnector</code> object represents a Twitter connector asset. This class is a sub-class of <a href="/web-services/api/asset-classes/connector"><code>Connector</code></a>.</p>
+<h2>Structure of <code>twitterConnector</code></h2>
+<pre>twitterConnector
+  id
+  name
+  parentContainerId
+  parentContainerPath
+  path
+  siteId
+  siteName
+  auth1
+  auth2
+  url
+  verified
+  verifiedDate
+  connectorParameters
+    connectorParameter
+      name
+      value
+  connectorContentTypeLinks
+    connectorContentTypeLink
+      contentTypeId
+      contentTypePath
+      pageConfigurationId
+      pageConfigurationName
+      connectorContentTypeLinkParams
+  destinationId
+  destinationPath
+</pre>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/twitter_connector.php">twitter_connector.php</a></li></ul></postscript>
 </documentation>
 */
 class TwitterConnector extends Connector
@@ -40,20 +68,20 @@ class TwitterConnector extends Connector
     }
 
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>destinationId</code>.</p></description>
+<example>echo $tc->getDestinationId(), BR;</example>
+<return-type>string </return-type>
 <exception></exception>
 </documentation>
 */
-    public function getDestinationId()
+    public function getDestinationId() : string
     {
         return $this->getProperty()->destinationId;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>Returns <code>destinationPath</code>.</p></description>
+<example>echo $tc->getDestinationPath(), BR;</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
@@ -64,9 +92,9 @@ class TwitterConnector extends Connector
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the value of the <code>connectorParameter</code> named <code>Hash Tags</code>.</p></description>
+<example>echo $tc->getHashTags(), BR;</example>
+<return-type>mixed</return-type>
 <exception></exception>
 </documentation>
 */
@@ -84,9 +112,9 @@ class TwitterConnector extends Connector
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the value of the <code>connectorParameter</code> named <code>Prefix</code>.</p></description>
+<example>echo $tc->getPrefix(), BR;</example>
+<return-type>mixed</return-type>
 <exception></exception>
 </documentation>
 */
@@ -104,13 +132,13 @@ class TwitterConnector extends Connector
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Sets the value of the <code>connectorParameter</code> named <code>Hash Tags</code>, and returns the calling object.</p></description>
+<example>$tc->setHashTags( '' )->setPrefix( '' )->edit();</example>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function setHashTags( $value )
+    public function setHashTags( string $value ) : Asset
     {
         $connector_parameters = $this->getConnectorParameters();
         
@@ -125,13 +153,13 @@ class TwitterConnector extends Connector
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Sets the value of the <code>connectorParameter</code> named <code>Prefix</code>, and returns the calling object.</p></description>
+<example>$tc->setHashTags( '' )->setPrefix( '' )->edit();</example>
+<return-type>Asset</return-type>
 <exception></exception>
 </documentation>
 */
-    public function setPrefix( $value )
+    public function setPrefix( string $value ) : Asset
     {
         $connector_parameters = $this->getConnectorParameters();
         
