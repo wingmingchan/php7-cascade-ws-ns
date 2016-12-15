e096f24ab613a9a6abb9b7783201a2c36a638ee2
Wing Ming Chan <chanw@upstate.edu>
2016-12-15 08:53:36 -0500

    Updated

--- cascade_ws_ns7/property_classes/AclEntry.class.php
+++ cascade_ws_ns7/property_classes/AclEntry.class.php
@@ -9,12 +9,70 @@
 namespace cascade_ws_property;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
+use cascade_ws_asset     as a;
 
+/**
+<documentation><description><h2>Introduction</h2>
+<p>An <code>AclEntry</code> object represents an <code>aclEntry</code> property found in an <code>AccessRightsInformation</code> object.</p>
+<h2>Structure of <code>aclEntry</code></h2>
+<pre>aclEntry
+  level
+  type
+  name
+</pre>
+<ul>
+<li>level: "read" or "write"</li>
+<li>type: "group" or "user"</li>
+<li>name: name of the group or user</li>
+</ul>
+<p>Example:</p>
+<pre>["aclEntry"]=&gt;
+    array(3) {
+      [0]=&gt;
+      object(stdClass)#70 (3) {
+        ["level"]=&gt;
+        string(4) "read"
+        ["type"]=&gt;
+        string(5) "group"
+        ["name"]=&gt;
+        string(13) "CWT-Designers"
+      }
+      [1]=&gt;
+      object(stdClass)#71 (3) {
+        ["level"]=&gt;
+        string(5) "write"
+        ["type"]=&gt;
+        string(4) "user"
+        ["name"]=&gt;
+        string(10) "chanw-test"
+      }
+      [2]=&gt;
+      object(stdClass)#68 (3) {
+        ["level"]=&gt;
+        string(5) "write"
+        ["type"]=&gt;
+        string(4) "user"
+        ["name"]=&gt;
+        string(5) "chanw"
+      }
+    }
+</pre>
+</description>
+<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+</documentation>
+*/
 class AclEntry extends Property
 {
+/**
+<documentation><description><p>The constructor.</p></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public function __construct( 
         \stdClass $ae=NULL, 
         aohs\AssetOperationHandlerService $service=NULL, 
@@ -30,6 +88,13 @@ class AclEntry extends Property
         }
     }
     
+/**
+<documentation><description><p>Display some information and returns the calling object.</p></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public function display()
     {
         echo "Level: " . $this->level . BR .
@@ -38,21 +103,49 @@ class AclEntry extends Property
         return $this;
     }
     
+/**
+<documentation><description><p>Returns <code>level</code>.</p></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public function getLevel()
     {
         return $this->level;
     }
     
+/**
+<documentation><description><p>Returns <code>name</code>.</p></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public function getName()
     {
         return $this->name;
     }
     
+/**
+<documentation><description><p>Returns <code>type</code>.</p></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public function getType()
     {
         return $this->type;
     }
     
+/**
+<documentation><description><p>Sets <code>level</code> and returns the calling object.</p></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public function setLevel( $level )
     {
         if( !c\LevelValues::isLevel( $level ) )
@@ -62,7 +155,13 @@ class AclEntry extends Property
         return $this;
     }
 
-    public function toStdClass()
+/**
+<documentation><description><p>Converts the object back to an <code>\stdClass</code> object.</p></description>
+<example></example>
+<return-type>stdClass</return-type>
+</documentation>
+*/
+    public function toStdClass() : \stdClass
     {
         $obj        = new \stdClass();
         $obj->level = $this->level;
