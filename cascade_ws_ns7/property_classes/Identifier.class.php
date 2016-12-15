52c75d40f322d44867029c029bd2193fe191d218
Wing Ming Chan <chanw@upstate.edu>
2016-12-15 12:39:12 -0500

    Updated

--- cascade_ws_ns7/property_classes/Identifier.class.php
+++ cascade_ws_ns7/property_classes/Identifier.class.php
@@ -12,6 +12,11 @@ use cascade_ws_constants as c;
 use cascade_ws_AOHS as aohs;
 use cascade_ws_utility as u;
 use cascade_ws_exception as e;
- 
+
+/**
+<documentation><description><h2>Introduction</h2>
+<p>The <code>Identifier</code> class is an empty sub-class of <a href="/web-services/api/property-classes/child"><code>Child</code></a>.</p>
+</description></documentation>
+*/
 class Identifier extends Child{}
 ?>
