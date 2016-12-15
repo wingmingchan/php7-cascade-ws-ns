6b7c3355efacf6bc9d7cd4c36cd730e59f38b04d
Wing Ming Chan <chanw@upstate.edu>
2016-12-15 12:42:16 -0500

    Updated

--- cascade_ws_ns7/property_classes/Parameter.class.php
+++ cascade_ws_ns7/property_classes/Parameter.class.php
@@ -9,12 +9,32 @@
 namespace cascade_ws_property;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
+use cascade_ws_asset     as a;
 
+/**
+<documentation><description><h2>Introduction</h2>
+<p>A <code>Parameter</code> object represents a <code>parameter</code> property found in a <a href="/web-services/api/property-classes/plugin"><code>Plugin</code></a> object in an <a href="web-services/api/asset-classes/asset-factory"><code>a\AssetFactory</code></a> object.</p>
+<h2>Structure of <code>parameter</code></h2>
+<pre>parameter
+  name
+  value
+</pre>
+</description>
+<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+</documentation>
+*/
 class Parameter extends Property
 {
+/**
+<documentation><description><p>The constructor.</p></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public function __construct( 
         \stdClass $p=NULL, 
         aohs\AssetOperationHandlerService $service=NULL, 
@@ -29,23 +49,47 @@ class Parameter extends Property
         }
     }
     
-    public function getName()
+/**
+<documentation><description><p>Returns <code>name</code>.</p></description>
+<example></example>
+<return-type>string</return-type>
+</documentation>
+*/
+    public function getName() : string
     {
         return $this->name;
     }
     
-    public function getValue()
+/**
+<documentation><description><p>Returns <code>value</code>.</p></description>
+<example></example>
+<return-type>string</return-type>
+</documentation>
+*/
+    public function getValue() : string
     {
         return $this->value;
     }
     
-    public function setValue( $value )
+/**
+<documentation><description><p>Sets <code>value</code> and returns the calling object.</p></description>
+<example></example>
+<return-type>Property</return-type>
+</documentation>
+*/
+    public function setValue( string $value ) : Property
     {
         $this->value = $value;
         return $this;
     }
     
-    public function toStdClass()
+/**
+<documentation><description><p>Converts the object back to an <code>\stdClass</code> object.</p></description>
+<example></example>
+<return-type>stdClass</return-type>
+</documentation>
+*/
+    public function toStdClass() : \stdClass
     {
         $obj        = new \stdClass();
         $obj->name  = $this->name;
