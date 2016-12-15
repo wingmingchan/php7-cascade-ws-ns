1e0d3b9d451202c9ab3e89085a4f681077d85dae
Wing Ming Chan <chanw@upstate.edu>
2016-12-15 08:56:24 -0500

    Updated

--- cascade_ws_ns7/property_classes/ConnectorParameter.class.php
+++ cascade_ws_ns7/property_classes/ConnectorParameter.class.php
@@ -13,5 +13,10 @@ use cascade_ws_AOHS as aohs;
 use cascade_ws_utility as u;
 use cascade_ws_exception as e;
 
+/**
+<documentation><description><h2>Introduction</h2>
+<p><code>ConnectorParameter</code> is an empty sub-class of <a href="/web-services/api/property-classes/parameter"><code>Parameter</code></a> used by <a href="web-services/api/asset-classes/connector"><code>Connector</code></a> and its sub-classes.</p></description>
+</documentation>
+*/
 class ConnectorParameter extends Parameter{}
 ?>
