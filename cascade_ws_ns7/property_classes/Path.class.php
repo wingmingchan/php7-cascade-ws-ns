b11bafa09ac718816f6b4240adc5bc5f8f62262d
Wing Ming Chan <chanw@upstate.edu>
2016-12-15 12:42:31 -0500

    Updated

--- cascade_ws_ns7/property_classes/Path.class.php
+++ cascade_ws_ns7/property_classes/Path.class.php
@@ -9,12 +9,37 @@
 namespace cascade_ws_property;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
+use cascade_ws_asset     as a;
 
+/**
+<documentation><description><h2>Introduction</h2>
+<p>A <code>Path</code> object represents a <code>path</code> (the parent) property found in a <a href="/web-services/api/property-classes/child"><code>Child</code></a> object inside a <a href="web-services/api/asset-classes/folder"><code>a\Folder</code></a> object.</p>
+<h2>Structure of <code>path</code></h2>
+<pre>path
+  path
+  siteId
+  siteName
+</pre>
+<h2>Design Issues</h2>
+<ul>
+<li>There are no <code>set</code> methods in this class.</li>
+</ul>
+</description>
+<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+</documentation>
+*/
 class Path extends Property
 {
+/**
+<documentation><description><p>The constructor.</p></description>
+<example></example>
+<return-type></return-type>
+<exception></exception>
+</documentation>
+*/
     public function __construct( 
         \stdClass $p=NULL, 
         aohs\AssetOperationHandlerService $service=NULL, 
@@ -34,22 +59,46 @@ class Path extends Property
         }
     }
     
+/**
+<documentation><description><p>Returns <code>path</code>.</p></description>
+<example></example>
+<return-type></return-type>
+</documentation>
+*/
     public function getPath()
     {
         return $this->path;
     }
     
+/**
+<documentation><description><p>Returns <code>siteId</code>.</p></description>
+<example></example>
+<return-type></return-type>
+</documentation>
+*/
     public function getSiteId()
     {
         return $this->site_id;
     }
     
+/**
+<documentation><description><p>Returns <code>siteName</code>.</p></description>
+<example></example>
+<return-type></return-type>
+</documentation>
+*/
     public function getSiteName()
     {
         return $this->site_name;
     }
 
-    public function toStdClass()
+/**
+<documentation><description><p>Converts the object back to an <code>\stdClass</code> object.</p></description>
+<example></example>
+<return-type>stdClass</return-type>
+</documentation>
+*/
+    public function toStdClass() : \stdClass
     {
         $obj           = new \stdClass();
         $obj->path     = $this->path;
