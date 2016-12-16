c9b58fa29d5a773c7a82286225b34950f0a4bd0a
Wing Ming Chan <chanw@upstate.edu>
2016-12-16 13:38:42 -0500

    Updated

--- cascade_ws_ns7/asset_classes/Message.class.php
+++ cascade_ws_ns7/asset_classes/Message.class.php
@@ -4,6 +4,7 @@
   * Copyright (c) 2016 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 12/16/2016 Fixed a bug related to $this->msg_errors.
   * 5/28/2015 Added namespaces.
   * 6/2/2014 Added asset expiration.
   * 5/22/2014 Fixed some bugs. Added republishFailedJob.
@@ -19,9 +20,19 @@ use cascade_ws_property as p;
 /**
 <documentation>
 <description><h2>Introduction</h2>
-
+<p>A <code>Message</code> object represents a message. This class is an independent class that does not extend another class.</p>
+<p>Messages can be classified into various types, depending on what is included in the subject line. Currently, this class supports six types: "Asset expiration", "Publish", "Un-publish", "Summary", "Workflow", and "Others". These types are used mainly in <a href="web-services/api/message-arrays"><code>MessageArrays</code></a> and <a href="web-services/api/cascade"><code>Cascade</code></a>.</p>
+<h2>Structure of <code>message</code></h2>
+<pre>message
+  id
+  to
+  from
+  subject
+  body
+  date
+</pre>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href=""></a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/message.php">message.php</a></li></ul></postscript>
 </documentation>
 */
 class Message
@@ -46,11 +57,9 @@ class Message
     const PATTERN_SKIPPED_JOBS_MSG1    = "/<a name=\"skippedjobs\"\/>\W+br.+\W+.+-+([\w\W]+)<a name=\"brokenlinks\"/";
     const PATTERN_SKIPPED_JOBS_MSG2    = "/<a name=\"skippedjobs\"\/>\W+br.+\W+.+-+([\w\W]+)<\/font/";
     const PATTERN_BROKEN_LINKS_MSG     = "/<a name=\"brokenlinks\"\/>\W+br.+\W+.+-+([\w\W]+)<\/font/";
-    
-    const PATTERN_ERRORS = '/<b>([^<]+)<\/b>/';
 
 /**
-<documentation><description><p></p></description>
+<documentation><description><p>The constructor.</p></description>
 <example></example>
 <return-type></return-type>
 <exception></exception>
@@ -66,6 +75,8 @@ class Message
             $this->date    = new \DateTime( $message->date );
             $this->subject = trim( $message->subject );
             $this->body    = $message->body;
+            
+            $this->msg_errors = array();
             
             if( u\StringUtility::startsWith( $this->subject, self::TYPE_EXPIRATION ) )
             {
@@ -103,7 +114,10 @@ class Message
             }
             else // not relevant to summary
             {
-                $this->with_issues = true;
+                if( $this->type == self::TYPE_SUMMARY )
+                    $this->with_issues = true;
+                else
+                    $this->with_issues = false;
             }
             
             if( strpos( $this->subject, "(0 failures)" ) !== false )
@@ -112,7 +126,10 @@ class Message
             }
             else
             {
-                $this->with_failures = true;
+                if( $this->type == self::TYPE_SUMMARY )
+                    $this->with_failures = true;
+                else
+                    $this->with_failures = false;
             }
             
             if( strpos( $this->subject, "is complete" ) !== false )
@@ -135,7 +152,7 @@ class Message
             }
             else
             {
-                $this->num_successful_jobs = NULL;
+                $this->num_successful_jobs = 0;
             }
             
             preg_match( self::PATTERN_JOBS_WITH_ERRORS, $this->body, $matches );
@@ -146,7 +163,7 @@ class Message
             }
             else
             {
-                $this->num_jobs_with_errors = NULL;
+                $this->num_jobs_with_errors = 0;
             }
             
             preg_match( self::PATTERN_SKIPPED_JOBS, $this->body, $matches );
@@ -157,7 +174,7 @@ class Message
             }
             else
             {
-                $this->num_skipped_jobs = NULL;
+                $this->num_skipped_jobs = 0;
             }
             
             preg_match( self::PATTERN_BROKEN_LINKS, $this->body, $matches );
@@ -168,7 +185,7 @@ class Message
             }
             else
             {
-                $this->num_broken_links = NULL;
+                $this->num_broken_links = 0;
             }
             
             preg_match( self::PATTERN_SUCCESSFUL_JOBS_MSG, $this->body, $matches );
@@ -186,7 +203,13 @@ class Message
             
             if( isset( $matches[ 1 ] ) )
             {
-                $this->msg_jobs_with_errors = $matches[ 1 ];
+                if( $this->num_jobs_with_errors > 0 )
+                {
+                    $this->msg_jobs_with_errors = $matches[ 1 ];
+                    $this->msg_errors[] = trim( $this->msg_jobs_with_errors );
+                }
+                else
+                    $this->msg_jobs_with_errors = "";
             }
             else
             {
@@ -200,7 +223,13 @@ class Message
             
             if( isset( $matches[ 1 ] ) )
             {
-                $this->msg_skipped_jobs = $matches[ 1 ];
+                if( $this->num_skipped_jobs > 0 )
+                {
+                    $this->msg_skipped_jobs = $matches[ 1 ];
+                    $this->msg_errors[] = trim( $this->msg_skipped_jobs );
+                }
+                else
+                    $this->msg_skipped_jobs = "";
             }
             else
             {
@@ -211,42 +240,29 @@ class Message
             
             if( isset( $matches[ 1 ] ) )
             {
-                $this->msg_broken_links = $matches[ 1 ];
+                if( $this->num_broken_links > 0 )
+                {
+                    $this->msg_broken_links = $matches[ 1 ];
+                    $this->msg_errors[] = trim( $this->msg_broken_links );
+                }
+                else
+                    $this->msg_broken_links = "";
             }
             else
             {
                 $this->msg_broken_links = "";
             }
-            
-            $this->msg_errors = array();
-            
-            preg_match_all( self::PATTERN_ERRORS, $this->body, $matches );
-            
-            if( isset( $matches[ 1 ] ) )
-            {
-                $errors = $matches[ 1 ];
-                
-                if( !is_array( $errors ) )
-                {
-                    $errors = array( $errors );
-                }
-                    
-                foreach( $errors as $error )
-                {
-                    $this->msg_errors[] = trim( $error );
-                }
-            }
         }
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>Displays the message and returns the calling object.</p></description>
+<example>$message->display();</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
 */
-    public function display()
+    public function display() : Message
     {
         echo S_H2      . "Message " . $this->id . E_H2 .
             c\L::TYPE    . $this->type . BR .
@@ -260,80 +276,80 @@ class Message
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>body</code>.</p></description>
+<example>echo $message->getBody(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getBody()
+    public function getBody() : string
     {
         return $this->body;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the "Broken Links" part of the message or an empty string.</p></description>
+<example>echo $message->getBrokenLinks(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getBrokenLinks()
+    public function getBrokenLinks() : string
     {
         return $this->msg_broken_links;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>date</code> (a <code>DateTime</code> object).</p></description>
+<example>echo date_format( $message->getDate(), 'Y-m-d H:i:s' ), BR;</example>
+<return-type>DateTime</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getDate()
+    public function getDate() : \DateTime
     {
         return $this->date;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns an array of error strings or an empty array.</p></description>
+<example>u\DebugUtility::dump( $message->getErrors() );</example>
+<return-type>array</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getErrors()
+    public function getErrors() : array
     {
         return $this->msg_errors;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>from</code>.</p></description>
+<example>echo $message->getFrom(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getFrom()
+    public function getFrom() : string
     {
         return $this->from;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>id</code>.</p></description>
+<example>echo $message->getId(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getId()
+    public function getId() : string
     {
         return $this->id;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>Returns a bool, indicating whether the job is complete. This value is only relevant to workflow messages.</p></description>
+<example>echo u\StringUtility::boolToString( $message->getIsComplete() ), BR;</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
@@ -344,8 +360,8 @@ class Message
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>Returns the "Jobs with Errors" part of the message or an empty string.</p></description>
+<example>echo $message->getJobsWithErrors(), BR;</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
@@ -356,145 +372,151 @@ class Message
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the number of broken links in the message.</p></description>
+<example>echo $message->getNumberOfBrokenLinks(), BR;</example>
+<return-type>int</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getNumberOfBrokenLinks()
+    public function getNumberOfBrokenLinks() : int
     {
         return $this->num_broken_links;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the number of jobs with errors in the message.</p></description>
+<example>echo $message->getNumberOfJobsWithErrors(), BR;</example>
+<return-type>int</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getNumberOfJobsWithErrors()
+    public function getNumberOfJobsWithErrors() : int
     {
         return $this->num_jobs_with_errors;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the number of skipped jobs in the message.</p></description>
+<example>echo $message->getNumberOfSkippedJobs(), BR;</example>
+<return-type>int</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getNumberOfSkippedJobs()
+    public function getNumberOfSkippedJobs() : int
     {
         return $this->num_skipped_jobs;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the number of successful jobs in the message.</p></description>
+<example>echo $message->getNumberOfSuccessfulJobs(), BR;</example>
+<return-type>int</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getNumberOfSuccessfulJobs()
+    public function getNumberOfSuccessfulJobs() : int
     {
         return $this->num_successful_jobs;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the "Skipped Jobs" part of the message or an empty string.</p></description>
+<example>echo $message->getSkippedJobs(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getSkippedJobs()
+    public function getSkippedJobs() : string
     {
         return $this->msg_skipped_jobs;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>subject</code>.</p></description>
+<example>echo $message->getSubject(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getSubject()
+    public function getSubject() : string
     {
         return $this->subject;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the "Successful Jobs" part of the message or an empty string.</p></description>
+<example>echo $message->getSuccessfulJobs(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getSuccessfulJobs()
+    public function getSuccessfulJobs() : string
     {
         return $this->msg_successful_jobs;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns <code>to</code>.</p></description>
+<example>echo $message->getTo(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getTo()
+    public function getTo() : string
     {
         return $this->to;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns the type string.</p></description>
+<example>echo $message->getType(), BR;</example>
+<return-type>string</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getType()
+    public function getType() : string
     {
         return $this->type;
     }
 
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns a bool, indicating whether the message is with failures. This value is only relevant to summary messages.</p></description>
+<example>echo u\StringUtility::boolToString( $message->getWithFailures() ), BR;</example>
+<return-type>bool</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getWithFailures()
+    public function getWithFailures() : bool
     {
         return $this->with_failures;
     }
 
 /**
-<documentation><description><p></p></description>
-<example></example>
-<return-type></return-type>
+<documentation><description><p>Returns a bool, indicating whether the message is with issues. This value is only relevant to publish and unpublish messages.</p></description>
+<example>echo u\StringUtility::boolToString( $message->getWithIssues() ), BR;</example>
+<return-type>bool</return-type>
 <exception></exception>
 </documentation>
 */
-    public function getWithIssues()
+    public function getWithIssues() : bool
     {
         return $this->with_issues;
     }
     
 /**
-<documentation><description><p></p></description>
-<example></example>
+<documentation><description><p>For a message of type "Publish" with failed jobs, this
+method analyses the error message and re-executes all failed jobs, and returns the calling
+object. There are two things worth noting. First, this method ignores the destination part
+of the message and republish to all destinations. Second, it assumes that all failed jobs
+are page-related. If the asset that failed to publish is not a page, it will be ignored by
+this method.</p></description>
+<example>$message->republishFailedJobs( $cascade );</example>
 <return-type></return-type>
 <exception></exception>
 </documentation>
 */
-    public function republishFailedJobs( Cascade $cascade, Destination $destination )
+    public function republishFailedJobs(
+        Cascade $cascade ) : Message
     {
         echo $this->type . BR;
         echo $this->num_jobs_with_errors . BR;
@@ -512,7 +534,8 @@ class Message
                     // ignore destinations
                     if( $site != "" && $path != "" )
                     {
-                        $cascade->getAsset( Page::TYPE, $path, $site )->publish( $destination );
+                        $cascade->getAsset( Page::TYPE, $path, $site )->
+                            publish( $destination );
                     }
                 }
                 catch( \Exception $e )
@@ -526,8 +549,15 @@ class Message
         return $this;
     }
 
-    /* for sorting, descending */
-    public static function compare( Message $m1, Message $m2 )
+/**
+<documentation><description><p>A method used to sort <code>Message</code> objects, using the <code>date</code> object. The sort order is descending.</p></description>
+<example></example>
+<return-type>int</return-type>
+<exception></exception>
+</documentation>
+*/
+
+    public static function compare( Message $m1, Message $m2 ) : int
     {
         if( $m1->getDate() == $m2->getDate() )
         {
