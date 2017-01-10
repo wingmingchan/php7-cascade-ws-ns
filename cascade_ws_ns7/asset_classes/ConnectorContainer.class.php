3916968518aed43d5a2f8b078b61ed8efe40277b
Wing Ming Chan <chanw@upstate.edu>
2017-01-10 10:37:46 -0500

    Added JSON structure and JSON dump

--- cascade_ws_ns7/asset_classes/ConnectorContainer.class.php
+++ cascade_ws_ns7/asset_classes/ConnectorContainer.class.php
@@ -4,22 +4,24 @@
   * Copyright (c) 2017 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 1/10/2017 Added JSON structure and JSON dump.
   * 5/28/2015 Added namespaces.
  */
 namespace cascade_ws_asset;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
-use cascade_ws_property as p;
+use cascade_ws_property  as p;
 
 /**
 <documentation>
 <description><h2>Introduction</h2>
 <p>A <code>ConnectorContainer</code> object represents a connector container asset. This class is a sub-class of <a href="http://www.upstate.edu/cascade-admin/web-services/api/asset-classes/container.php"><code>Container</code></a>.</p>
 <h2>Structure of <code>connectorContainer</code></h2>
-<pre>connectorContainer
+<pre>SOAP:
+connectorContainer
   id
   name
   parentContainerId
@@ -36,9 +38,49 @@ use cascade_ws_property as p;
         siteName
       type
       recycled
+
+JSON:
+connectorContainer
+  children (array)
+    stdClass
+      id
+      path (stdClass)
+        path
+        siteId
+      type
+      recycled
+  parentContainerId
+  parentContainerPath
+  path
+  siteId
+  siteName
+  name
+  id
 </pre>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/connector_container.php">connector_container.php</a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/connector_container.php">connector_container.php</a></li></ul>
+<h2>JSON Dump</h2>
+<pre>{ "asset":{
+  "connectorContainer":{
+    "children":[
+    {
+      "id":"086439518b7ffe8339ce5d13b34124b6",
+      "path":{
+        "path":"Test Connector Container/Google",
+        "siteId":"1f2172088b7ffe834c5fe91e9596d028" },
+      "type":"googleanalyticsconnector",
+      "recycled":false } ],
+    "parentContainerId":"1f2177b48b7ffe834c5fe91e1a7d31f4",
+    "parentContainerPath":"/",
+    "path":"Test Connector Container",
+    "siteId":"1f2172088b7ffe834c5fe91e9596d028",
+    "siteName":"cascade-admin-webapp",
+    "name":"Test Connector Container",
+    "id":"03dbe3628b7ffe8339ce5d132b740004" } },
+  "success":true
+}
+</pre>
+</postscript>
 </documentation>
 */
 class ConnectorContainer extends Container
