1e877033f6e2df6b21c0beae26889f2c179558af
Wing Ming Chan <chanw@upstate.edu>
2017-01-12 13:56:46 -0500

    Added JSON structure and JSON dump

--- cascade_ws_ns7/asset_classes/FeedBlock.class.php
+++ cascade_ws_ns7/asset_classes/FeedBlock.class.php
@@ -4,6 +4,7 @@
   * Copyright (c) 2017 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 1/12/2017 Added JSON structure and JSON dump.
   * 10/24/2016 Added construtor.
   * 5/28/2015 Added namespaces.
  */
@@ -20,7 +21,8 @@ use cascade_ws_property  as p;
 <description><h2>Introduction</h2>
 <p>A <code>FeedBlock</code> object represents a feed block asset. This class is a sub-class of <a href="http://www.upstate.edu/cascade-admin/web-services/api/asset-classes/block.php"><code>Block</code></a>.</p>
 <h2>Structure of <code>feedBlock</code></h2>
-<pre>feedBlock
+<pre>SOAP structure:
+feedBlock
   id
   name
   parentFolderId
@@ -55,8 +57,71 @@ use cascade_ws_property  as p;
   expirationFolderPath
   expirationFolderRecycled
   feedURL
+  
+JSON structure:
+feedBlock
+  feedURL
+  expirationFolderId
+  expirationFolderPath
+  expirationFolderRecycled (bool)
+  metadataSetId
+  metadataSetPath
+  metadata
+    author
+    displayName
+    endDate
+    keywords
+    metaDescription
+    reviewDate
+    startDate
+    summary
+    teaser
+    title
+    dynamicFields (array)
+      stdClass
+        name
+        fieldValues (array)
+          stdClass
+            value
+  parentFolderId
+  parentFolderPath
+  lastModifiedDate
+  lastModifiedBy
+  createdDate
+  createdBy
+  path
+  siteId
+  siteName
+  name
+  id
 </pre></description>
-<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/feed_block.php">feed_block.php</a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/feed_block.php">feed_block.php</a></li></ul>
+<h2>JSON Dump</h2>
+<pre>{ "asset":{
+  "feedBlock":{
+    "feedURL":"http://www.upstate.edu/news/",
+    "expirationFolderRecycled":false,
+    "metadataSetId":"358be6af8b7ffe83164c9314f9a3c1a6",
+    "metadataSetPath":"_common_assets:Block",
+    "metadata":{
+      "dynamicFields":[ {
+        "name":"macro",
+        "fieldValues":[ { "value":"" } ] } ] },
+    "parentFolderId":"1f22ab188b7ffe834c5fe91eed1a064a",
+    "parentFolderPath":"_cascade/blocks/feed",
+    "lastModifiedDate":"Sep 12, 2016 12:01:57 PM",
+    "lastModifiedBy":"wing",
+    "createdDate":"Sep 12, 2016 12:01:57 PM",
+    "createdBy":"wing",
+    "path":"_cascade/blocks/feed/hannonhill-sandbox-wing-asset",
+    "siteId":"1f2172088b7ffe834c5fe91e9596d028",
+    "siteName":"cascade-admin-webapp",
+    "name":"hannonhill-sandbox-wing-asset",
+    "id":"1f22332a8b7ffe834c5fe91e33ecd4c7"}},
+  "success":true
+}
+</pre>
+</postscript>
 </documentation>
 */
 class FeedBlock extends Block
