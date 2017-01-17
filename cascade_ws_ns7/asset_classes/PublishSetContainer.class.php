f0a9834189b94252430dd6423cb431aa22d26fab
Wing Ming Chan <chanw@upstate.edu>
2017-01-17 12:24:14 -0500

    Added JSON structure and JSON dump

--- cascade_ws_ns7/asset_classes/PublishSetContainer.class.php
+++ cascade_ws_ns7/asset_classes/PublishSetContainer.class.php
@@ -4,6 +4,7 @@
   * Copyright (c) 2017 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 1/17/2017 Added JSON structure and JSON dump.
   * 5/28/2015 Added namespaces.
  */
 namespace cascade_ws_asset;
@@ -19,7 +20,8 @@ use cascade_ws_property  as p;
 <description><h2>Introduction</h2>
 <p>A <code>PublishSetContainer</code> object represents a publish set container asset. This class is a sub-class of <a href="/web-services/api/asset-classes/container"><code>Container</code></a>.</p>
 <h2>Structure of <code>publishSetContainer</code></h2>
-<pre>publishSetContainer
+<pre>SOAP:
+publishSetContainer
   id
   name
   parentContainerId
@@ -36,9 +38,42 @@ use cascade_ws_property  as p;
         siteName
       type
       recycled
+
+JSON:
+publishSetContainer
+  children (array)
+    stdClass
+      id
+      path
+        path
+        siteId
+      type
+      recycled
+  parentContainerId
+  parentContainerPath
+  path
+  siteId
+  siteName
+  name
+  id
 </pre>
 </description>
-<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/publish_set_container.php">publish_set_container.php</a></li></ul></postscript>
+<postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/publish_set_container.php">publish_set_container.php</a></li></ul>
+<h2>JSON Dump</h2>
+<pre>{ "asset":{
+  "publishSetContainer":{
+    "children":[],
+    "parentContainerId":"1f21774c8b7ffe834c5fe91e130d561a",
+    "parentContainerPath":"/",
+    "path":"Test Publish Set Container",
+    "siteId":"1f2172088b7ffe834c5fe91e9596d028",
+    "siteName":"cascade-admin-webapp",
+    "name":"Test Publish Set Container",
+    "id":"ad6ebf308b7ffe83664b9bd4760644e6" } },
+  "success":true
+}
+</pre>
+</postscript>
 </documentation>
 */
 class PublishSetContainer extends Container
