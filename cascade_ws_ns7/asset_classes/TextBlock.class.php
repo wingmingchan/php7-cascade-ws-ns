8c91a669a8ffe1a95202591aba3fb48686015b5a
Wing Ming Chan <chanw@upstate.edu>
2017-01-17 14:32:44 -0500

    Added JSON structure and JSON dump

--- cascade_ws_ns7/asset_classes/TextBlock.class.php
+++ cascade_ws_ns7/asset_classes/TextBlock.class.php
@@ -4,6 +4,7 @@
   * Copyright (c) 2017 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 1/17/2017 Added JSON structure and JSON dump.
   * 10/24/2016 Fixed a namespace, added constructor.
   * 5/28/2015 Added namespaces.
  */
@@ -22,7 +23,8 @@ use cascade_ws_property as p;
 <p>A <code>TextBlock</code> object represents a text block asset. This class is a sub-class of
 <a href="http://www.upstate.edu/cascade-admin/web-services/api/asset-classes/block.php"><code>Block</code></a>.</p>
 <h2>Structure of <code>textBlock</code></h2>
-<pre>textBlock
+<pre>SOAP:
+textBlock
   id
   name
   parentFolderId
@@ -57,7 +59,43 @@ use cascade_ws_property as p;
   expirationFolderPath
   expirationFolderRecycled
   text
-</pre>
+  
+JSON:
+textBlock
+  text
+  expirationFolderId
+  expirationFolderPath
+  expirationFolderRecycled
+  metadataSetId
+  metadataSetPath
+  metadata
+    author
+    displayName
+    endDate
+    keywords
+    metaDescription
+    reviewDate
+    startDate
+    summary
+    teaser
+    title
+    dynamicFields (array)
+      stdClass
+        name
+        fieldValues (array)
+          stdClass
+            value
+  parentFolderId
+  parentFolderPath
+  lastModifiedDate
+  lastModifiedBy
+  createdDate
+  createdBy
+  path
+  siteId
+  siteName
+  name
+  id</pre>
 </description>
 <postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/text_block.php">text_block.php</a></li></ul>
 <h2>JSON Dump</h2>
