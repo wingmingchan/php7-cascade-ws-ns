282b606d1a86712c8f82593a432624936fd931a0
Wing Ming Chan <chanw@upstate.edu>
2017-06-23 08:04:57 -0400

    Replaced static WSDL code with call to getXMLFragments

--- cascade_ws_ns7/asset_classes/FtpTransport.class.php
+++ cascade_ws_ns7/asset_classes/FtpTransport.class.php
@@ -4,6 +4,7 @@
   * Copyright (c) 2017 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 6/23/2017 Replaced static WSDL code with call to getXMLFragments.
   * 6/13/2017 Added WSDL.
   * 1/17/2017 Added JSON dump.
   * 5/28/2015 Added namespaces.
@@ -19,8 +20,10 @@ use cascade_ws_property  as p;
 
 /**
 <documentation>
-<description><h2>Introduction</h2>
-<p>An <code>FtpTransport</code> object represents a database transport asset. This class is a sub-class of <a href="/web-services/api/asset-classes/transport"><code>Transport</code></a>.</p>
+<description>
+<?php global $service;
+$doc_string = "<h2>Introduction</h2>
+<p>An <code>FtpTransport</code> object represents a database transport asset. This class is a sub-class of <a href=\"/web-services/api/asset-classes/transport\"><code>Transport</code></a>.</p>
 <h2>Structure of <code>ftpTransport</code></h2>
 <pre>ftpTransport
   id
@@ -38,23 +41,13 @@ use cascade_ws_property  as p;
   doSFTP
   doPASV
 </pre>
-<h2>WSDL</h2>
-<pre>&lt;complexType name="ftpTransport">
-  &lt;complexContent>
-    &lt;extension base="impl:containered-asset">
-      &lt;sequence>
-        &lt;element maxOccurs="1" minOccurs="1" name="hostName" type="xsd:string"/>
-        &lt;element maxOccurs="1" minOccurs="1" name="port" type="xsd:positiveInteger"/>
-        &lt;element maxOccurs="1" minOccurs="1" name="username" type="xsd:string"/>
-        &lt;element maxOccurs="1" minOccurs="1" name="password" type="xsd:string"/>
-        &lt;element maxOccurs="1" minOccurs="0" name="directory" type="xsd:string"/>
-        &lt;element maxOccurs="1" minOccurs="0" name="doSFTP" type="xsd:boolean"/>
-        &lt;element maxOccurs="1" minOccurs="0" name="doPASV" type="xsd:boolean"/>
-      &lt;/sequence>
-    &lt;/extension>
-  &lt;/complexContent>
-&lt;/complexType>
-</pre>
+<h2>WSDL</h2>";
+$doc_string .=
+    $service->getXMLFragments( array(
+        array( "getComplexTypeXMLByName" => "ftpTransport" ),
+    ) );
+return $doc_string;
+?>
 </description>
 <postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/ftp_transport.php">ftp_transport.php</a></li></ul>
 <h2>JSON Dump</h2>
