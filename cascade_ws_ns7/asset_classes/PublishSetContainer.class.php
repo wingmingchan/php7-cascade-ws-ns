40bdb85060602eaa79e7b0e5c6f62dba1593bd69
Wing Ming Chan <chanw@upstate.edu>
2017-06-28 13:26:23 -0400

    Replaced static WSDL code with call to getXMLFragments

--- cascade_ws_ns7/asset_classes/PublishSetContainer.class.php
+++ cascade_ws_ns7/asset_classes/PublishSetContainer.class.php
@@ -4,6 +4,7 @@
   * Copyright (c) 2017 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 6/28/2017 Replaced static WSDL code with call to getXMLFragments.
   * 6/13/2017 Added WSDL.
   * 1/17/2017 Added JSON structure and JSON dump.
   * 5/28/2015 Added namespaces.
@@ -18,8 +19,10 @@ use cascade_ws_property  as p;
 
 /**
 <documentation>
-<description><h2>Introduction</h2>
-<p>A <code>PublishSetContainer</code> object represents a publish set container asset. This class is a sub-class of <a href="/web-services/api/asset-classes/container"><code>Container</code></a>.</p>
+<description>
+<?php global $service;
+$doc_string = "<h2>Introduction</h2>
+<p>A <code>PublishSetContainer</code> object represents a publish set container asset. This class is a sub-class of <a href=\"/web-services/api/asset-classes/container\"><code>Container</code></a>.</p>
 <h2>Structure of <code>publishSetContainer</code></h2>
 <pre>SOAP:
 publishSetContainer
@@ -58,17 +61,16 @@ publishSetContainer
   name
   id
 </pre>
-<h2>WSDL</h2>
-<pre>&lt;complexType name="publishSetContainer">
-  &lt;complexContent>
-    &lt;extension base="impl:containered-asset">
-      &lt;sequence>
-        &lt;element maxOccurs="1" minOccurs="0" name="children" nillable="true" type="impl:container-children"/>
-      &lt;/sequence>
-    &lt;/extension>
-  &lt;/complexContent>
-&lt;/complexType>
-</pre>
+<h2>WSDL</h2>";
+$doc_string .=
+    $service->getXMLFragments( array(
+        array( "getComplexTypeXMLByName" => "publishSetContainer" ),
+        array( "getComplexTypeXMLByName" => "container-children" ),
+        array( "getComplexTypeXMLByName" => "identifier" ),
+        array( "getComplexTypeXMLByName" => "path" ),
+    ) );
+return $doc_string;
+?>
 </description>
 <postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/publish_set_container.php">publish_set_container.php</a></li></ul>
 <h2>JSON Dump</h2>
