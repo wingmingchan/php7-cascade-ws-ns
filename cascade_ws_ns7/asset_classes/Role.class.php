742f62cc0ce1ac684d204ecf84d84eaf4ce8ff88
Wing Ming Chan <chanw@upstate.edu>
2017-06-29 11:18:12 -0400

    Removed JSON dump. Updated documentation.

--- cascade_ws_ns7/asset_classes/Role.class.php
+++ cascade_ws_ns7/asset_classes/Role.class.php
@@ -4,6 +4,8 @@
   * Copyright (c) 2017 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 6/28/2017 Replaced static WSDL code with call to getXMLFragments.
+  * Removed JSON dump. Updated documentation.
   * 6/12/2017 Added WSDL.
   * 1/17/2017 Added JSON dump.
   * 5/28/2015 Added namespaces.
@@ -18,198 +20,35 @@ use cascade_ws_property  as p;
 
 /**
 <documentation>
-<description><h2>Introduction</h2>
+<description>
+<?php global $service;
+$doc_string = "<h2>Introduction</h2>
 <p>A <code>Role</code> object represents a role asset.</p>
-<p>There are two types of roles in Cascade: global and site. In a <code>role</code> property, there are two sub-properties: <code>globalAbilities</code> and <code>siteAbilities</code>. For a global role, the <code>siteAbilities</code> property stores a <code>NULL</code> value. For a site role, the <code>globalAbilities</code> property stores a <code>NULL</code> value. Corresponding to these two properties, there are two classes: <a href="site://cascade-admin-old/projects/web-services/oop/classes/property-classes/global-abilities"><code>p\GlobalAbilities</code></a> and <a href="site://cascade-admin-old/projects/web-services/oop/classes/property-classes/site-abilities"><code>p\SiteAbilities</code></a>. These two classes are sub-classes of the <a href="site://cascade-admin-old/projects/web-services/oop/classes/property-classes/abilities"><code>p\Abilities</code></a> class. A <code>Role</code> object has a <code>p\GlobalAbilities</code> object and a <code>p\SiteAbilities</code> object.</p>
+<p>There are two types of roles in Cascade: global and site. In a <code>role</code> property, there are two sub-properties: <code>globalAbilities</code> and <code>siteAbilities</code>. For a global role, the <code>siteAbilities</code> property stores a <code>NULL</code> value. For a site role, the <code>globalAbilities</code> property stores a <code>NULL</code> value. Corresponding to these two properties, there are two classes: <a href=\"site://cascade-admin/web-services/api/property-classes/global-abilities\"><code>p\GlobalAbilities</code></a> and <a href=\"site://cascade-admin/web-services/api/property-classes/site-abilities\"><code>p\SiteAbilities</code></a>. A <code>Role</code> object has a <code>p\GlobalAbilities</code> object and a <code>p\SiteAbilities</code> object.</p>
 <h2>Structure of <code>role</code></h2>
 <pre>role
   id
   name
   roleType
   globalAbilities
-    (82 properties, v.8)
+    (36 properties, v.8.4.1)
   siteAbilities
-    (51 properties)
+    (48 properties, v.8.4.1)
 </pre>
 <h2>Design Issues</h2>
-<p>Since there are too many methods (85 <code>get</code> and 85 <code>set</code> methods) involved here, I decide not to repeat these methods in various classes. Instead, I provide two <code>get</code> methods, i.e., <code>getGlobalAbilities()</code> and <code>getSiteAbilities()</code> in this class, each returning an <code>Abilities</code> object, allowing us to manipulate these two objects directly. Therefore, there are no <code>set</code> methods in this class.</p>
-<h2>WSDL</h2>
-<pre>&lt;complexType name="role">
-  &lt;complexContent>
-    &lt;extension base="impl:named-asset">
-      &lt;sequence>
-        &lt;element maxOccurs="1" minOccurs="1" name="roleType" type="impl:role-types"/>
-        &lt;choice>
-          &lt;element maxOccurs="1" minOccurs="1" name="globalAbilities" type="impl:global-abilities"/>
-          &lt;element maxOccurs="1" minOccurs="1" name="siteAbilities" type="impl:site-abilities"/>
-        &lt;/choice>
-      &lt;/sequence>
-    &lt;/extension>
-  &lt;/complexContent>
-&lt;/complexType>
-
-&lt;simpleType name="role-types">
-  &lt;restriction base="xsd:string">
-    &lt;enumeration value="site"/>
-    &lt;enumeration value="global"/>
-  &lt;/restriction>
-&lt;/simpleType>
-</pre>
+<p>Since there are too many methods (84 <code>get</code> and 84 <code>set</code> methods) involved here, I decide not to repeat these methods in various classes. Instead, I provide two <code>get</code> methods, i.e., <code>getGlobalAbilities()</code> and <code>getSiteAbilities()</code> in this class, returning a <code>GlobalAbilities</code> object and a <code>SiteAbilities</code> object respectively, allowing us to manipulate these two objects directly. Therefore, there are no <code>set</code> methods in this class.</p>
+<h2>WSDL</h2>";
+$doc_string .=
+    $service->getXMLFragments( array(
+        array( "getComplexTypeXMLByName" => "role" ),
+        array( "getSimpleTypeXMLByName"  => "role-types" ),
+        array( "getComplexTypeXMLByName" => "global-abilities" ),
+        array( "getComplexTypeXMLByName" => "site-abilities" ),
+    ) );
+return $doc_string;
+?>
 </description>
 <postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/role.php">role.php</a></li></ul>
-<h2>JSON Dump</h2>
-<pre>
-{ "asset":{
-  "role":{
-    "roleType":"global",
-    "globalAbilities":{
-      "bypassAllPermissionsChecks":false,
-      "accessSiteManagement":false,
-      "createSites":false,
-      "uploadImagesFromWysiwyg":false,
-      "multiSelectCopy":false,
-      "multiSelectPublish":false,
-      "multiSelectMove":false,
-      "multiSelectDelete":false,
-      "editPageLevelConfigurations":false,
-      "editPageContentType":false,
-      "editDataDefinition":false,
-      "publishReadableHomeAssets":false,
-      "publishWritableHomeAssets":false,
-      "viewPublishQueue":true,
-      "reorderPublishQueue":false,
-      "cancelPublishJobs":false,
-      "editAccessRights":false,
-      "viewVersions":false,
-      "activateDeleteVersions":false,
-      "accessAudits":false,
-      "bypassWorkflow":false,
-      "assignApproveWorkflowSteps":false,
-      "deleteWorkflows":false,
-      "breakLocks":false,
-      "assignWorkflowsToFolders":false,
-      "bypassAssetFactoryGroupsNewMenu":false,
-      "bypassDestinationGroupsWhenPublishing":false,
-      "bypassWorkflowDefintionGroupsForFolders":false,
-      "alwaysAllowedToToggleDataChecks":false,
-      "accessManageSiteArea":false,
-      "accessAdminArea":false,
-      "accessAssetFactories":false,
-      "accessConfigurationSets":false,
-      "accessDataDefinitions":false,
-      "accessMetadataSets":false,
-      "accessPublishSets":false,
-      "accessTargetsDestinations":false,
-      "accessTransports":false,
-      "accessWorkflowDefinitions":false,
-      "accessContentTypes":false,
-      "accessAllSites":true,
-      "viewSystemInfoAndLogs":false,
-      "forceLogout":false,
-      "diagnosticTests":false,
-      "accessSecurityArea":false,
-      "publishReadableAdminAreaAssets":false,
-      "publishWritableAdminAreaAssets":false,
-      "integrateFolder":false,
-      "importZipArchive":false,
-      "optimizeDatabase":false,
-      "syncLdap":false,
-      "bulkChange":false,
-      "configureLogging":false,
-      "searchingIndexing":false,
-      "accessConfiguration":false,
-      "editSystemPreferences":false,
-      "broadcastMessages":false,
-      "recycleBinViewRestoreUserAssets":false,
-      "recycleBinDeleteAssets":false,
-      "recycleBinViewRestoreAllAssets":false,
-      "viewUsersInMemberGroups":false,
-      "viewAllUsers":false,
-      "createUsers":false,
-      "deleteUsersInMemberGroups":false,
-      "deleteAllUsers":false,
-      "viewMemberGroups":false,
-      "viewAllGroups":false,
-      "createGroups":false,
-      "deleteMemberGroups":false,
-      "accessRoles":false,
-      "createRoles":false,
-      "deleteAnyGroup":false,
-      "editAnyUser":false,
-      "editUsersInMemberGroups":false,
-      "editAnyGroup":false,
-      "editMemberGroups":false,
-      "changeIdentity":false,
-      "moveRenameAssets":false,
-      "databaseExportTool":false,
-      "sendStaleAssetNotifications":false,
-      "brokenLinkReportAccess":false,
-      "brokenLinkReportMarkFixed":false },
-    "name":"Default",
-    "id":"10" } },
-  "success":true
-}
-
-{ "asset":{
-  "role":{
-    "roleType":"site",
-    "siteAbilities":{
-      "bypassAllPermissionsChecks":false,
-      "uploadImagesFromWysiwyg":false,
-      "multiSelectCopy":true,
-      "multiSelectPublish":true,
-      "multiSelectMove":true,
-      "multiSelectDelete":true,
-      "editPageLevelConfigurations":true,
-      "editPageContentType":true,
-      "editDataDefinition":true,
-      "publishReadableHomeAssets":true,
-      "publishWritableHomeAssets":true,
-      "editAccessRights":false,
-      "viewVersions":true,
-      "activateDeleteVersions":true,
-      "accessAudits":true,
-      "bypassWorkflow":true,
-      "assignApproveWorkflowSteps":true,
-      "deleteWorkflows":true,
-      "breakLocks":true,
-      "assignWorkflowsToFolders":true,
-      "bypassAssetFactoryGroupsNewMenu":true,
-      "bypassDestinationGroupsWhenPublishing":false,
-      "bypassWorkflowDefintionGroupsForFolders":true,
-      "accessManageSiteArea":true,
-      "accessAssetFactories":true,
-      "accessConfigurationSets":false,
-      "accessDataDefinitions":true,
-      "accessMetadataSets":true,
-      "accessPublishSets":true,
-      "accessDestinations":false,
-      "accessTransports":false,
-      "accessWorkflowDefinitions":true,
-      "accessContentTypes":false,
-      "accessConnectors":true,
-      "publishReadableAdminAreaAssets":true,
-      "publishWritableAdminAreaAssets":false,
-      "integrateFolder":true,
-      "importZipArchive":true,
-      "bulkChange":true,
-      "recycleBinViewRestoreUserAssets":true,
-      "recycleBinDeleteAssets":true,
-      "recycleBinViewRestoreAllAssets":true,
-      "moveRenameAssets":true,
-      "diagnosticTests":false,
-      "alwaysAllowedToToggleDataChecks":true,
-      "viewPublishQueue":true,
-      "reorderPublishQueue":true,
-      "cancelPublishJobs":true,
-      "sendStaleAssetNotifications":true,
-      "brokenLinkReportAccess":false,
-      "brokenLinkReportMarkFixed":false },
-    "name":"Library-Site-Administrator",
-    "id":"131" } },
-  "success":true
-}
-</pre>
 </postscript>
 </documentation>
 */
