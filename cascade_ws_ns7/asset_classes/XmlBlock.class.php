3365e9e2a54218d9faf48a8595e4dfb671f8fdc4
Wing Ming Chan <chanw@upstate.edu>
2017-06-30 15:25:57 -0400

    Replaced static WSDL code with call to getXMLFragments

--- cascade_ws_ns7/asset_classes/XmlBlock.class.php
+++ cascade_ws_ns7/asset_classes/XmlBlock.class.php
@@ -4,6 +4,7 @@
   * Copyright (c) 2017 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 6/30/2017 Replaced static WSDL code with call to getXMLFragments.
   * 6/13/2017 Added WSDL.
   * 1/17/2017 Added JSON structure and JSON dump.
   * 10/24/2016 Added construtor.
@@ -20,8 +21,10 @@ use cascade_ws_property  as p;
 
 /**
 <documentation>
-<description><h2>Introduction</h2>
-<p>An <code>XmlBlock</code> object represents an an xml block asset. This class is a sub-class of <a href="/web-services/api/asset-classes/block"><code>Block</code></a>.</p>
+<description>
+<?php global $service;
+$doc_string = "<h2>Introduction</h2>
+<p>An <code>XmlBlock</code> object represents an an xml block asset. This class is a sub-class of <a href=\"/cascade-admin/web-services/api/asset-classes/block.php\"><code>Block</code></a>.</p>
 <h2>Structure of <code>xmlBlock</code></h2>
 <pre>SOAP:
 xmlBlock
@@ -96,17 +99,13 @@ xmlBlock
   siteName
   name
   id</pre>
-<h2>WSDL</h2>
-<pre>&lt;complexType name="xmlBlock">
-  &lt;complexContent>
-    &lt;extension base="impl:block">
-      &lt;sequence>
-        &lt;element maxOccurs="1" minOccurs="1" name="xml" type="xsd:string"/>
-      &lt;/sequence>
-    &lt;/extension>
-  &lt;/complexContent>
-&lt;/complexType>
-</pre>
+<h2>WSDL</h2>";
+$doc_string .=
+    $service->getXMLFragments( array(
+        array( "getComplexTypeXMLByName" => "xmlBlock" ),
+    ) );
+return $doc_string;
+?>
 </description>
 <postscript><h2>Test Code</h2><ul><li><a href="https://github.com/wingmingchan/php-cascade-ws-ns-examples/blob/master/asset-class-test-code/xml_block.php">xml_block.php</a></li></ul>
 <h2>JSON Dump</h2>
