81017c88f514232b9a9dd4e0c7ebb91f39b1a56c
Wing Ming Chan <chanw@upstate.edu>
2017-06-30 14:35:06 -0400

    Updated

--- cascade_ws_ns7/asset_classes/Folder.class.php
+++ cascade_ws_ns7/asset_classes/Folder.class.php
@@ -34,7 +34,7 @@ use cascade_ws_property  as p;
 <description>
 <?php global $service;
 $doc_string = "<h2>Introduction</h2>
-<p>A <code>Folder</code> object represents a folder asset. This class is a sub-class of <a href=\"/web-services/api/asset-classes/container\"><code>Container</code></a>.</p>
+<p>A <code>Folder</code> object represents a folder asset. This class is a sub-class of <a href=\"/cascade-admin/web-services/api/asset-classes/container.php\"><code>Container</code></a>.</p>
 <h2>Structure of <code>folder</code></h2>
 <pre>SOAP:
 folder
