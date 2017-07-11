dfd38c846c36ed519449c9ee203fb87e2d2c5a03
Wing Ming Chan <chanw@upstate.edu>
2017-07-11 15:03:09 -0400

    Replaced static WSDL code with call to getXMLFragments

--- cascade_ws_ns7/property_classes/ConnectorParameter.class.php
+++ cascade_ws_ns7/property_classes/ConnectorParameter.class.php
@@ -4,33 +4,30 @@
   * Copyright (c) 2017 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 7/11/2017 Replaced static WSDL code with call to getXMLFragments.
   * 6/13/2017 Added WSDL.
   * 5/28/2015 Added namespaces.
  */
 namespace cascade_ws_property;
 
 use cascade_ws_constants as c;
-use cascade_ws_AOHS as aohs;
-use cascade_ws_utility as u;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
 use cascade_ws_exception as e;
 
 /**
-<documentation><description><h2>Introduction</h2>
-<p><code>ConnectorParameter</code> is an empty sub-class of <a href="/web-services/api/property-classes/parameter"><code>Parameter</code></a> used by <a href="web-services/api/asset-classes/connector"><code>Connector</code></a> and its sub-classes.</p>
-<h2>WSDL</h2>
-<pre>&lt;complexType name="connector-parameter-list">
-  &lt;sequence>
-    &lt;element maxOccurs="unbounded" minOccurs="0" name="connectorParameter" nillable="true" type="impl:connector-parameter"/>
-  &lt;/sequence>
-&lt;/complexType>
-
-&lt;complexType name="connector-parameter">
-  &lt;sequence>
-    &lt;element maxOccurs="1" minOccurs="1" name="name" nillable="true" type="xsd:string"/>
-    &lt;element maxOccurs="1" minOccurs="1" name="value" nillable="true" type="xsd:string"/>
-  &lt;/sequence>
-&lt;/complexType>
-</pre>
+<documentation><description>
+<?php global $service;
+$doc_string = "<h2>Introduction</h2>
+<p><code>ConnectorParameter</code> is an empty sub-class of <a href=\"/cascade-admin/web-services/api/property-classes/parameter.php\"><code>Parameter</code></a> used by <a href=\"/cascade-admin/web-services/api/asset-classes/connector.php\"><code>Connector</code></a> and its sub-classes.</p>
+<h2>WSDL</h2>";
+$doc_string .=
+    $service->getXMLFragments( array(
+        array( "getComplexTypeXMLByName" => "connector-parameter-list" ),
+        array( "getComplexTypeXMLByName" => "connector-parameter" ),
+    ) );
+return $doc_string;
+?>
 </description>
 </documentation>
 */
