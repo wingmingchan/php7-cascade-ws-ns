e3a3d8478957ff91a878f51b5d27ff9137b8d806
Wing Ming Chan <chanw@upstate.edu>
2017-08-07 15:09:21 -0400

    Updated

--- cascade_ws_ns7/auth_user.php
+++ cascade_ws_ns7/auth_user.php
@@ -10,12 +10,22 @@ $auth           = new \stdClass();
 $auth->username = $_SERVER['PHP_AUTH_USER'];
 $auth->password = $_SERVER['PHP_AUTH_PW'];
 
+$context = stream_context_create( [
+    'ssl' => [
+    	// set some SSL/TLS specific options
+    	'verify_peer'       => false,
+    	'verify_peer_name'  => false,
+    	'allow_self_signed' => true
+    ]
+]);
+
 try
 {
     // set up global objects
     $service = new aohs\AssetOperationHandlerService( $wsdl, $auth );
     $cascade = new a\Cascade( $service );
     $report  = new a\Report( $cascade );
+    $eval    = new u\EvalUtility();
 
     // create an asset for one-time use
     $asset = new \stdClass();
