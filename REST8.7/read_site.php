840bd5618f0d4a248bd38d55f068643727088d8e
Wing Ming Chan <chanw@upstate.edu>
2017-11-29 09:37:20 -0500

    Initial commit

---
+++ REST8.7/read_site.php
@@ -0,0 +1,23 @@
+<?php
+require_once( 'auth_rest_webapp.php' );
+
+use cascade_ws_constants as c;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
+
+try
+{
+    $type  = "site";
+    $id    = "c12d8c498b7ffe83129ed6d81ea4076a";
+    $asset = $service->read( $service->createId( $type, $id ) );
+    u\DebugUtility::dump( $asset );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
+}
+?>
