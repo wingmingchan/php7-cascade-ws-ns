c166aadc8422bb84d9b857ec2ccc5c14d3e25cd4
Wing Ming Chan <chanw@upstate.edu>
2017-11-29 09:25:52 -0500

    Initial commit

---
+++ REST8.7/auth_rest_user.php
@@ -0,0 +1,25 @@
+<?php
+require_once( 'constants.php' );
+require_once( 'AssetOperationHandlerService.class.php' );
+require_once( 'DebugUtility.class.php' );
+
+use cascade_ws_AOHS    as aohs;
+
+$url     = "http://www.mydomain.edu:1234/api/v1/";
+$auth    = new \stdClass();
+$auth->u = "username";
+$auth->p = "password";
+
+try
+{
+	$service = new aohs\AssetOperationHandlerService( $url, $auth );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE; 
+}
+catch( \Error $er )
+{
+	echo S_PRE . $er . E_PRE; 
+}
+?>
