80b4bbf1965a4b98366b4254cc5a007b45542c2f
Wing Ming Chan <chanw@upstate.edu>
2017-11-30 08:55:51 -0500

    Initial commit

---
+++ REST8.7/site_copy.php
@@ -0,0 +1,22 @@
+<?php
+require_once( 'auth_rest_webapp.php' );
+
+use cascade_ws_constants as c;
+use cascade_ws_AOHS      as aohs;
+use cascade_ws_utility   as u;
+
+try
+{
+    $reply = $service->siteCopy(
+        "6a8d58418b7ffe83164c9314aed51787", "_rwd_seed", "_rwd_seed_copy" );
+    u\DebugUtility::dump( $reply );
+}
+catch( \Exception $e ) 
+{
+    echo S_PRE . $e . E_PRE;
+}
+catch( \Error $er )
+{
+    echo S_PRE . $er . E_PRE;
+}
+?>
