71b74b94ce413a5e67e5e362647e010dd1ef7fb4
Wing Ming Chan <chanw@upstate.edu>
2018-01-19 10:10:19 -0500

    Updated

--- cascade_ws_ns7/AssetOperationHandlerService.class.php
+++ cascade_ws_ns7/AssetOperationHandlerService.class.php
@@ -1,10 +1,11 @@
 <?php
 /**
-  Author: Wing Ming Chan
-  * Copyright (c) 2018 Wing Ming Chan <chanw@upstate.edu>, 
+  Author: Wing Ming Chan, German Drulyk
+  Copyright (c) 2018 Wing Ming Chan <chanw@upstate.edu>, 
                        German Drulyk <drulykg@upstate.edu>
   MIT Licensed
   Modification history:
+  1/17/2018 Moved $properties and $types from child classes to here.
   1/12/2018 Created the parent class.
  */
 namespace cascade_ws_AOHS;
@@ -18,9 +19,9 @@ use cascade_ws_exception as e;
 /**
 <documentation>
 <description><?php global $eval, $service;
-$doc_string = "
-<h2>Introduction</h2>
-<p>This class encapsulates the WSDL URL, the authentication object, and the SoapClient object, and provides services of all operations defined in the WSDL. There are 28 operations defined in the WSDL:</p>
+$doc_string = "<h2>Introduction</h2>
+<p>This class is an abstract class and the parent class of <code>AssetOperationHandlerServiceRest</code> and <code>AssetOperationHandlerServiceSoap</code>. The two child classes encapsulate 
+the WSDL URL, authentication information, and in the case of <code>AssetOperationHandlerServiceSoap</code>, the SoapClient object, and provide services of all operations defined in the WSDL. There are 28 operations defined in the WSDL:</p>
 <ul>
 <li>batch</li>
 <li>checkIn</li>
@@ -50,23 +51,36 @@ $doc_string = "
 <li>search</li>
 <li>sendMessage (deprecated)</li>
 <li>siteCopy</li>
-</ul><p>All 28 operations have been encapsulated in this class. The general format of a method encapsulating an operation is the following:</p>
+</ul><p>All 28 operations have been encapsulated in these classes (the <code>batch</code> operation is not implemented in REST by Hannon Hill). The general format of a method encapsulating an operation is the following:</p>
 <ol>
 <li>Create the parameters for the operation</li>
-<li>Call the corresponding operation through the SOAP client</li>
-<li>Store the results</li>
+<li>Call the corresponding operation through REST/the SOAP client</li>
 </ol>
-<p>Here is the code of <code>batch</code>, for example:</p>
+<p>Here is the code of <code>read</code>, for example:</p>
 <pre>
-    function batch( array \$operations )
+    // REST
+    public function read( \stdClass \$identifier )
     {
-        \$batch_param                 = new \stdClass();
-        \$batch_param->authentication = \$this->auth;
-        \$batch_param->operation      = \$operations;
+        \$id_string = \$this->createIdString( \$identifier );
+        \$command   = \$this->url . __function__ . '/' . \$id_string . \$this->auth;
+        \$this->reply   = \$this->apiOperation( \$command );
+        \$this->success = \$this->reply->success;
         
-        \$this->reply = \$this->soapClient->batch( \$batch_param );
-        // the returned object is an array
-        \$this->storeResults();
+        return \$this->reply->asset ?? NULL;
+    }
+
+    // SOAP
+    public function read( \stdClass \$identifier ) : \stdClass
+    {
+        if( self::DEBUG ) { u\DebugUtility::dump( \$identifier ); }
+        
+        \$read_param                 = new \stdClass();
+        \$read_param->authentication = \$this->auth;
+        \$read_param->identifier     = \$identifier;
+        
+        \$this->reply = \$this->soapClient->read( \$read_param );
+        \$this->storeResults( \$this->reply->readReturn );
+        return \$this->reply;
     }
 </pre>
 <p>Besides encapsulating the 28 operations, there are also other utility methods:</p>
@@ -82,7 +96,6 @@ $doc_string .= "<h3>Simple Types</h3>";
 $doc_string .= $service->getSimpleTypeNameList();
 $doc_string .= "<h3>Complex Types</h3>";
 $doc_string .= $service->getComplexTypeNameList();
-//$doc_string .= "<h3>authentication</h3>";
 $doc_string .=
     $service->getXMLFragments( array(
         array( "getComplexTypeXMLByName" => "authentication" ),
@@ -90,8 +103,6 @@ $doc_string .=
         array( "getComplexTypeXMLByName" => "path" ),
         array( "getComplexTypeXMLByName" => "operation" ),
     ) );
-
-
 $doc_string .= "<h3>Operation Result</h3><pre>";
 $doc_string .=
     $eval->replaceBrackets($service->getComplexTypeXMLByName("operationResult"));
@@ -110,7 +121,7 @@ return $doc_string; ?></description>
 */
 abstract class AssetOperationHandlerService
 {
-	const NAME_SPACE  = "cascade_ws_AOHS";
+    const NAME_SPACE  = "cascade_ws_AOHS";
     const REST_STRING = "rest";
     const SOAP_STRING = "soap";
     
@@ -126,6 +137,26 @@ abstract class AssetOperationHandlerService
         {
             $this->service_type = trim( $type );
         }
+    }
+    
+/**
+<documentation><description><p>Returns the private array named <code>$properties</code>.</p></description>
+<example></example>
+<return-type>array</return-type></documentation>
+*/
+    public function getProperties() : array
+    {
+        return $this->properties;
+    }
+    
+/**
+<documentation><description><p>Returns the private array named <code>$types</code>.</p></description>
+<example></example>
+<return-type>array</return-type></documentation>
+*/
+    public function getTypes() : array
+    {
+        return $this->types;
     }
 
 /**
@@ -157,7 +188,7 @@ abstract class AssetOperationHandlerService
     }
 
 /**
-<documentation><description><p>Returns <code>false</code>.</p></description>
+<documentation><description><p>Returns a bool, indicating whether the object is associated with REST.</p></description>
 <example>echo u\StringUtility::boolToString( $service->isRest() );</example>
 <return-type>bool</return-type></documentation>
 */
@@ -167,7 +198,7 @@ abstract class AssetOperationHandlerService
     }
 
 /**
-<documentation><description><p>Returns <code>true</code>.</p></description>
+<documentation><description><p>Returns a bool, indicating whether the object is associated with SOAP.</p></description>
 <example>echo u\StringUtility::boolToString( $service->isSoap() );</example>
 <return-type>bool</return-type></documentation>
 */
@@ -177,5 +208,104 @@ abstract class AssetOperationHandlerService
     }
     
     private $service_type;
+    
+    // property array to generate methods
+    /*@var array The array of property names */
+    private $properties = array(
+        c\P::ASSETFACTORY,
+        c\P::ASSETFACTORYCONTAINER,
+        C\P::CLOUDTRANSPORT,
+        c\P::CONNECTORCONTAINER,
+        c\P::CONTENTTYPE,
+        c\P::CONTENTTYPECONTAINER,
+        c\P::DATADEFINITION,
+        c\P::DATADEFINITIONCONTAINER,
+        c\P::DATABASETRANSPORT,
+        c\P::DESTINATION,
+        c\P::FACEBOOKCONNECTOR,
+        c\P::FEEDBLOCK,
+        c\P::FILE,
+        c\P::FILESYSTEMTRANSPORT,
+        c\P::FOLDER,
+        c\P::FTPTRANSPORT,
+        c\P::GOOGLEANALYTICSCONNECTOR,
+        c\P::GROUP,
+        c\P::INDEXBLOCK,
+        c\P::METADATASET,
+        c\P::METADATASETCONTAINER,
+        c\P::PAGE,
+        c\P::PAGECONFIGURATIONSET,
+        c\P::PAGECONFIGURATIONSETCONTAINER,
+        c\P::PUBLISHSET,
+        c\P::PUBLISHSETCONTAINER,
+        c\P::REFERENCE,
+        c\P::ROLE,
+        c\P::SCRIPTFORMAT,
+        c\P::SITE,
+        c\P::SITEDESTINATIONCONTAINER,
+        c\P::SYMLINK,
+        c\P::TARGET,
+        c\P::TEMPLATE,
+        c\P::TEXTBLOCK,
+        c\P::TRANSPORTCONTAINER,
+        c\P::USER,
+        c\P::WORDPRESSCONNECTOR,
+        c\P::WORKFLOWDEFINITION,
+        c\P::WORKFLOWDEFINITIONCONTAINER,
+        c\P::XHTMLDATADEFINITIONBLOCK,
+        c\P::XMLBLOCK,
+        c\P::XSLTFORMAT
+    );
+    
+    /*@var array The array of types of assets */
+    private $types = array(
+        c\T::ASSETFACTORY,
+        c\T::ASSETFACTORYCONTAINER,
+        c\T::CLOUDTRANSPORT,
+        c\T::CONNECTORCONTAINER,
+        c\T::CONTENTTYPE,
+        c\T::CONTENTTYPECONTAINER,
+        c\T::DATADEFINITION,
+        c\T::DATADEFINITIONCONTAINER,
+        c\T::DESTINATION,
+        c\T::FACEBOOKCONNECTOR,
+        c\T::FEEDBLOCK,
+        c\T::FILE,
+        c\T::FOLDER,
+        c\T::GOOGLEANALYTICSCONNECTOR,
+        c\T::GROUP,
+        c\T::INDEXBLOCK,
+        c\T::MESSAGE,
+        c\T::METADATASET,
+        c\T::METADATASETCONTAINER,
+        c\T::PAGE,
+        c\T::PAGECONFIGURATION,
+        c\T::PAGECONFIGURATIONSET,
+        c\T::PAGECONFIGURATIONSETCONTAINER,
+        c\T::PAGEREGION,
+        c\T::PUBLISHSET,
+        c\T::PUBLISHSETCONTAINER,
+        c\T::REFERENCE,
+        c\T::ROLE,
+        c\T::SCRIPTFORMAT,
+        c\T::SITE,
+        c\T::SITEDESTINATIONCONTAINER,
+        c\T::SYMLINK,
+        c\T::TARGET,
+        c\T::TEMPLATE,
+        c\T::TEXTBLOCK,
+        c\T::TRANSPORTDB,
+        c\T::TRANSPORTFS,
+        c\T::TRANSPORTFTP,
+        c\T::TRANSPORTCONTAINER,
+        c\T::USER,
+        c\T::WORDPRESSCONNECTOR,
+        c\T::WORKFLOW,
+        c\T::WORKFLOWDEFINITION,
+        c\T::WORKFLOWDEFINITIONCONTAINER,
+        c\T::XHTMLDATADEFINITIONBLOCK,
+        c\T::XMLBLOCK,
+        c\T::XSLTFORMAT
+    );
 }
 ?>
