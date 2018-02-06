c0fd878f16f9e1cf689ddd95661461b8d09b0ae6
Wing Ming Chan <chanw@upstate.edu>
2018-02-06 12:44:40 -0500

    Updated

--- cascade_ws_ns7/asset_classes/DataDefinitionBlock.class.php
+++ cascade_ws_ns7/asset_classes/DataDefinitionBlock.class.php
@@ -4,6 +4,8 @@
   * Copyright (c) 2018 Wing Ming Chan <chanw@upstate.edu>
   * MIT Licensed
   * Modification history:
+  * 2/6/2018 Added removePhantomNodes (of type B).
+  * 2/5/2018 Added removePhantomValues.
   * 1/24/2018 Updated documentation.
   * 8/1/2017 Added getNodeBlock.
   * 6/20/2017 Replaced static WSDL code with call to getXMLFragments.
@@ -1381,8 +1383,10 @@ chooser node, allowing users to choose a page.</p></description>
         {
             $this->checkStructuredData();
             $new_sd = $this->structured_data->mapData();
+            
             return $this->setStructuredData( $new_sd );
         }
+        u\DebugUtility::out( "Soap is required for this to work" );
         return $this;
     }
 
@@ -1400,6 +1404,56 @@ qualified identifier of the first node of the set.</p></description>
         $this->checkStructuredData();
         $this->structured_data->removeLastSibling( $identifier );
         $this->edit();
+        return $this;
+    }
+    
+/**
+<documentation><description><p>Removes phantom nodes of type B in the block.</p></description>
+<example>$block->removePhantomNodes();</example>
+<return-type>Asset</return-type>
+<exception>WrongBlockTypeException</exception>
+</documentation>
+*/
+    public function removePhantomNodes( array &$results=NULL ) : Asset
+    {
+        $this->checkStructuredData();
+        // type B
+        if( $this->structured_data->hasPhantomNodes() )
+        {
+        	u\DebugUtility::out( "calling mapData" );
+        	
+            $this->mapData();
+            
+            if( isset( $results ) )
+            {
+            	$results[ self::TYPE ][ "B" ][] = $this->getPath();
+            }
+        }
+        return $this;
+    }
+    
+/**
+<documentation><description><p>Removes phantom values in the block.</p></description>
+<example>$block->removePhantomValues();</example>
+<return-type>Asset</return-type>
+<exception>WrongBlockTypeException</exception>
+</documentation>
+*/
+    public function removePhantomValues( array &$results=NULL ) : Asset
+    {
+        $this->checkStructuredData();
+        
+        if( $this->structured_data->hasPhantomValues() )
+        {
+            $this->structured_data->removePhantomValues();
+            
+            if( isset( $results ) )
+            {
+            	$results[ self::TYPE ][] = $this->getPath();
+            }
+            
+            return $this->edit();
+        }
         return $this;
     }
     
