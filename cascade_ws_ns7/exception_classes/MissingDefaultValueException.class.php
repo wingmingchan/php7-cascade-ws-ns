eae3cab388ee7c5375bfe3ca9a5a1da5b7a838d2
Wing Ming Chan <chanw@upstate.edu>
2018-02-06 12:43:18 -0500

    Initial commit

---
+++ cascade_ws_ns7/exception_classes/MissingDefaultValueException.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+  * Author: Wing Ming Chan
+  * Copyright (c) 2018 Wing Ming Chan <chanw@upstate.edu>
+  * MIT Licensed
+  * Modification history:
+  * 2/5/2018 Class created
+ */
+namespace cascade_ws_exception;
+
+class MissingDefaultValueException extends \Exception{}
+?>
