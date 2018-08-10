7b167896b028212684ea203a1bbc3d99cdc794c7
Wing Ming Chan <chanw@upstate.edu>
2018-08-10 09:56:23 -0400

    Updated

--- cascade_ws_ns7/AssetOperationHandlerService.class.php
+++ cascade_ws_ns7/AssetOperationHandlerService.class.php
@@ -5,6 +5,7 @@
                        German Drulyk <drulykg@upstate.edu>
   MIT Licensed
   Modification history:
+  7/19/2018 Moved WSDL-related constants and methods from child class to here.
   1/17/2018 Moved $properties and $types from child classes to here.
   1/12/2018 Created the parent class.
  */
@@ -51,7 +52,7 @@ the WSDL URL, authentication information, and in the case of <code>AssetOperatio
 <li>search</li>
 <li>sendMessage (deprecated)</li>
 <li>siteCopy</li>
-</ul><p>All 28 operations have been encapsulated in these classes (the <code>batch</code> operation is not implemented in REST by Hannon Hill). The general format of a method encapsulating an operation is the following:</p>
+</ul><p>All 28 operations have been encapsulated in these classes (the <code>batch</code> operation, as of Cascade 8.9, is not implemented in REST by Hannon Hill). The general format of a method encapsulating an operation is the following:</p>
 <ol>
 <li>Create the parameters for the operation</li>
 <li>Call the corresponding operation through REST/the SOAP client</li>
@@ -62,13 +63,19 @@ the WSDL URL, authentication information, and in the case of <code>AssetOperatio
     public function read( \stdClass \$identifier )
     {
         \$id_string = \$this->createIdString( \$identifier );
-        \$command   = \$this->url . __function__ . '/' . \$id_string . \$this->auth;
+        \$command   = \$this->url . __function__ . '/' . \$id_string;
+        
+        if( \$this->auth_in_content === false )
+        {
+            \$command .= \$this->auth;
+        }
+        
         \$this->reply   = \$this->apiOperation( \$command );
         \$this->success = \$this->reply->success;
         
         return \$this->reply->asset ?? NULL;
     }
-
+    
     // SOAP
     public function read( \stdClass \$identifier ) : \stdClass
     {
@@ -122,9 +129,26 @@ return $doc_string; ?></description>
 abstract class AssetOperationHandlerService
 {
     const NAME_SPACE  = "cascade_ws_AOHS";
+    // strings used to switch between REST and SOAP
     const REST_STRING = "rest";
     const SOAP_STRING = "soap";
     
+    // these constants are used to retrieve parts of the WSDL
+    const BINDING_PATH = "//wsdl:definitions/wsdl:binding";
+    const COMPLEX_TYPE_PATH =
+        "//wsdl:definitions/wsdl:types/schema:schema/schema:complexType";
+    const ELEMENT_PATH     = "//wsdl:definitions/wsdl:types/schema:schema/schema:element";
+    const MESSAGE_PATH     = "//wsdl:definitions/wsdl:message";
+    const PORT_TYPE_PATH   = "//wsdl:definitions/wsdl:portType";
+    const SIMPLE_TYPE_PATH =
+        "//wsdl:definitions/wsdl:types/schema:schema/schema:simpleType";
+    
+/**
+<documentation><description><p>The constructor. Do not call this constructor directly. Instead, use the <code>ServiceFactory::getService</code> method to get the instance of one of the child classes.</p>
+</description>
+<example>$service = aohs\ServiceFactory::getService( $type, $url, $username, $password );</example>
+</documentation>
+*/
     public function __construct(
         string $type, string $url, \stdClass $auth, $context=NULL )
     {
@@ -133,15 +157,117 @@ abstract class AssetOperationHandlerService
             throw new e\UnacceptableServiceTypeException(
                 "The type string $type is not acceptable." );
         }
-        else
+        else // store the type string
         {
             $this->service_type = trim( $type );
         }
+        
+        if( $this->isSoap() )
+        {
+            $wsdl = file_get_contents( $url );
+        }
+        // for REST, the url has to be calculated
+        elseif( $this->isRest() )
+        {
+            $wsdl_url = str_replace( 'api/v1/', '', $url ) .
+            "ws/services/AssetOperationService?wsdl";
+            $wsdl     = file_get_contents( $wsdl_url );
+        }
+            
+        $domDoc   = new \DOMDocument();
+        $domDoc->loadXML( $wsdl );
+        
+        $this->dom_xpath = new \DOMXpath( $domDoc );
+        $this->dom_xpath->registerNamespace( 'wsdl', 'http://schemas.xmlsoap.org/wsdl/' );
+        $this->dom_xpath->registerNamespace(
+            'schema', 'http://www.w3.org/2001/XMLSchema' );
     }
-    
+
 /**
-<documentation><description><p>Returns the private array named <code>$properties</code>.</p></description>
+<documentation><description><p>Returns the XML of <code>wsdl:binding</code>.</p></description>
+<example>echo $eval->replaceBrackets( $service->getBinding() );</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getBinding() : string
+    {
+        return $this->getXMLByPath( self::BINDING_PATH );
+    }
+
+/**
+<documentation><description><p>Returns a list of complex type names.</p></description>
+<example>echo $service->getComplexTypeNameList();</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getComplexTypeNameList() : string
+    {
+        return $this->getNameList( self::COMPLEX_TYPE_PATH );
+    }
+
+/**
+<documentation><description><p>Returns the XML of the named complex type.</p></description>
+<example>echo $eval->replaceBrackets( $service->getComplexTypeXMLByName( "copyParameters" ) );</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getComplexTypeXMLByName( string $name ) : string
+    {
+        return $this->getXMLByName( self::COMPLEX_TYPE_PATH, $name );
+    }
+
+/**
+<documentation><description><p>Returns the <code>DOMXpath</code> object storing the WSDL.</p></description>
 <example></example>
+<return-type>DOMXpath</return-type></documentation>
+*/
+    public function getDOMXpath() : \DOMXpath
+    {
+        return $this->dom_xpath;
+    }
+
+/**
+<documentation><description><p>Returns a list of element names.</p></description>
+<example>echo $eval->replaceBrackets( $service->getComplexTypeXMLByName( "copyParameters" ) );</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getElementNameList() : string
+    {
+        return $this->getNameList( self::ELEMENT_PATH );
+    }
+
+/**
+<documentation><description><p>Returns the XML of the named element.</p></description>
+<example>echo $eval->replaceBrackets( $service->getElementXMLByName( "deleteMessage" ) );</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getElementXMLByName( string $name ) : string
+    {
+        return $this->getXMLByName( self::ELEMENT_PATH, $name );
+    }
+
+/**
+<documentation><description><p>Returns the XML of <code>wsdl:message</code>.</p></description>
+<example>echo $eval->replaceBrackets( $service->getMessages() );</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getMessages() : string
+    {
+        return $this->getXMLByPath( self::MESSAGE_PATH );
+    }
+
+/**
+<documentation><description><p>Returns the XML of <code>wsdl:portType</code>.</p></description>
+<example>echo $eval->replaceBrackets( $service->getPortType() );</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getPortType() : string
+    {
+        return $this->getXMLByPath( self::PORT_TYPE_PATH );
+    }
+
+/**
+<documentation><description><p>Returns the private array named <code>$properties</code>.
+This array stores names of properties defined in the WSDL. These names are always
+in camelCase.</p></description>
+<example>u\DebugUtility::dump( $service->getProperties() );</example>
 <return-type>array</return-type></documentation>
 */
     public function getProperties() : array
@@ -149,16 +275,6 @@ abstract class AssetOperationHandlerService
         return $this->properties;
     }
     
-/**
-<documentation><description><p>Returns the private array named <code>$types</code>.</p></description>
-<example></example>
-<return-type>array</return-type></documentation>
-*/
-    public function getTypes() : array
-    {
-        return $this->types;
-    }
-
 /**
 <documentation><description><p>Returns a string of either 'soap' or 'rest'.</p></description>
 <example>echo $service->getServiceType();</example>
@@ -169,6 +285,67 @@ abstract class AssetOperationHandlerService
         return $this->service_type;
     }
     
+/**
+<documentation><description><p>Returns a list of simple type names.</p></description>
+<example>echo $service->getSimpleTypeNameList();</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getSimpleTypeNameList() : string
+    {
+        return $this->getNameList( self::SIMPLE_TYPE_PATH );
+    }
+
+/**
+<documentation><description><p>Returns the XML of the named complex type.</p></description>
+<example>echo $eval->replaceBrackets( $service->getSimpleTypeXMLByName( "message-mark-type" ) );</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getSimpleTypeXMLByName( string $name ) : string
+    {
+        return $this->getXMLByName( self::SIMPLE_TYPE_PATH, $name );
+    }
+
+/**
+<documentation><description><p>Returns the private array named <code>$types</code>.
+This array stores type strings defined in the WSDL.</p></description>
+<example>u\DebugUtility::dump( $service->getTypes() );</example>
+<return-type>array</return-type></documentation>
+*/
+    public function getTypes() : array
+    {
+        return $this->types;
+    }
+
+/**
+<documentation><description><p>Returns the concatenated XML fragments, based on the
+supplied list of method names and element names.</p></description>
+<example>$doc_string .=
+    $service->getXMLFragments( array(
+        array( "getComplexTypeXMLByName" => "entity-type" ),
+        array( "getSimpleTypeXMLByName"  => "entityTypeString" ),
+    ) );
+return $doc_string;
+</example>
+<return-type>string</return-type></documentation>
+*/
+    public function getXMLFragments( array $array ) : string
+    {
+        $doc_string = S_PRE;
+        $str_array  = array();
+        
+        foreach( $array as $sub_array )
+        {
+            foreach( $sub_array as $key => $value )
+            {
+                $str_array[] = u\XMLUtility::replaceBrackets( $this->$key( $value ) );
+            }
+        }
+        
+        $doc_string .= trim( implode( "\r", $str_array ), "\r" );
+        $doc_string .= E_PRE;
+        return $doc_string;
+    }
+
 /**
 <documentation><description><p>Returns a bool indicating whether the string is a 32-digit hex string.</p></description>
 <example>if( $service->isHexString( $id ) )
@@ -207,7 +384,66 @@ abstract class AssetOperationHandlerService
         return $this->service_type === self::SOAP_STRING;
     }
     
+    // helper functions
+    private function getNameList( string $path ) : string
+    {
+        $nodes = $this->dom_xpath->evaluate( $path );
+        $list  = "<ul>";
+        $names = array();
+        
+        if( sizeof( $nodes ) > 0 )
+        {
+            for( $i = 0; $i < $nodes->length; $i++ )
+            {
+                $names[] = $nodes->item( $i )->getAttribute( "name" );
+            }
+            
+            asort( $names );
+            
+            //u\DebugUtility::dump( $names );
+            
+            foreach( $names as $name )
+            {
+                $list .= "<li>$name</li>";
+            }
+        }
+        
+        $list .= "</ul>";
+        return $list;
+    }
+    private function getXMLByName( string $path, string $name ) : string
+    {
+        $xpath_str = $path . "[@name='$name']";
+        $nodes     = $this->dom_xpath->evaluate( $xpath_str );
+        $xml_str   = "";
+        
+        if( $nodes->length > 0 )
+        {
+            $xml_str = $nodes[ 0 ]->ownerDocument->saveXML( $nodes[ 0 ] );
+        }
+        else
+        {
+            // not found
+        }
+
+        return $xml_str;
+    }
+
+    private function getXMLByPath( string $path_str ) : string
+    {
+        $elements  = $this->dom_xpath->evaluate( $path_str );
+        $xml_str   = "";
+        
+        if( sizeof( $elements ) > 0 )
+        {
+            foreach( $elements as $element )
+                $xml_str .= $element->ownerDocument->saveXML( $element );
+        }
+        return $xml_str;
+    }
+
     private $service_type;
+    private $dom_xpath;
     
     // property array to generate methods
     /*@var array The array of property names */
